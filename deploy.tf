be312f523c393a3b835c8f528faa3678e7b9398a
Michele Bologna <michele.bologna@suse.com>
2016-06-24 17:01:34 +0200

    Initial import

---
+++ deploy.tf
@@ -0,0 +1,13 @@
+variable "count" {
+  default = 2
+}
+resource "openstack_compute_instance_v2" "web" {
+  count = "${var.count}"
+  name = "${format("web-%02d", count.index+1)}"
+  image_name = "SLES12-SP2"
+  flavor_id = "2"
+  security_groups = ["default"]
+  network {
+     uuid = "8cce38fd-443f-4b87-8ea5-ad2dc184064f"
+  }
+}
