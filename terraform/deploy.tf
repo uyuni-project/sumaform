5b0143df81584ae8acbbbc16c3bf2582b32b7557
Michele Bologna <michele.bologna@suse.com>
2016-06-29 09:22:25 +0200

    Rename instance to sumaform-test

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -2,9 +2,10 @@ variable "count" {
   default = 2
 }
 
-resource "openstack_compute_instance_v2" "web" {
   count = "${var.count}"
   name = "${format("web-%02d", count.index+1)}"
+resource "openstack_compute_instance_v2" "sumaform-test" {
+  name = "sumaform-test"
   image_name = "test-sumaform-sp11"
   flavor_id = "2"
   security_groups = ["default"]
