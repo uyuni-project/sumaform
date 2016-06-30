4e300ee1bb4bc1d18395228e2eb2d6149f63864f
Silvio Moioli <smoioli@suse.de>
2016-06-30 07:43:39 +0200

    Add support for provisioning with salt

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -1,6 +1,7 @@
 variable "count" {
   default = 2
 }
+
 resource "openstack_compute_instance_v2" "web" {
   count = "${var.count}"
   name = "${format("web-%02d", count.index+1)}"
@@ -9,5 +10,30 @@ resource "openstack_compute_instance_v2" "web" {
   security_groups = ["default"]
   network {
      uuid = "8cce38fd-443f-4b87-8ea5-ad2dc184064f"
+  }
+
+  provisioner "file" {
+    source = "salt"
+    destination = "/root"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo \"${template_file.grains.rendered}\" >> /etc/salt/grains",
+      "salt-call --force-color --file-root /root/salt --local state.sls terraform-support",
+      "salt-call --force-color --file-root /root/salt --local state.highstate"
+    ]
+  }
+}
+
+resource "template_file" "grains" {
+  template = "${file("grains")}"
+
+  vars {
+    hostname = "suma21pg"
+    avahi-domain = "${var.avahi-domain}"
+    version = "2.1-nightly"
+    database = "postgres"
+    role = "suse-manager-server"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,12 @@ variable "tenant_network" {
     description = "The network to be used."
     default  = "8cce38fd-443f-4b87-8ea5-ad2dc184064f"
 }
+
+variable "avahi-domain" {
+  default = "vagrant.local"
+}
+
+variable "package-mirror" {
+  default = "package-mirror"
+}
+
