f16a1b7a10364b97d987a840f038f1bd89573ff8
Silvio Moioli <smoioli@suse.de>
2016-07-01 17:14:45 +0200

    Cleanup OpenStack variables

--- openstack_host/variables.tf
+++ openstack_host/variables.tf
@@ -1,23 +1,14 @@
 variable "openstack_tenant_name" {
-    description = "The name of the Tenant."
-    default  = "openstack"
+  description = "The name of the Tenant (eg: `openstack`)."
 }
 
 variable "openstack_auth_url" {
-    description = "The endpoint url to connect to OpenStack."
-    default  = "***REMOVED***.0"
+  description = "The endpoint url to connect to OpenStack (eg: `***REMOVED***.0`)."
 }
 
 variable "tenant_network" {
-    description = "The network to be used."
-    default  = "8cce38fd-443f-4b87-8ea5-ad2dc184064f"
+  description = "The network to be used (eg. `8cce38fd-443f-4b87-8ea5-ad2dc184064f`)."
 }
-
-# TODO: instead of root/vagrant
-#variable "openstack_keypair" {
-#    description = "The keypair to be used."
-#    default  = ""
-#}
 
 variable "avahi-domain" {
   default = "vagrant.local"
