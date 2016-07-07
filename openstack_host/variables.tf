6c74dfd4dd96aa36786e47765cafe73dc192ab23
Michele Bologna <michele.bologna@suse.com>
2016-07-07 19:21:25 +0200

    Restore default variable declaration, adding help

--- openstack_host/variables.tf
+++ openstack_host/variables.tf
@@ -1,13 +1,11 @@
 variable "openstack_tenant_name" {
-  description = "The name of the Tenant (eg: `openstack`)."
+  description = "set this variable or export OS_TENANT_NAME"
+  #default = "openstack"
 }
 
 variable "openstack_auth_url" {
-  description = "The endpoint url to connect to OpenStack (eg: `***REMOVED***.0`)."
-}
-
-variable "tenant_network" {
-  description = "The network to be used (eg. `8cce38fd-443f-4b87-8ea5-ad2dc184064f`)."
+  description = "set this variable or export OS_AUTH_URL"
+  #default = "***REMOVED***.0"
 }
 
 variable "avahi-domain" {
