8f0211fd7ba915e52c8d189aed2e4064034bfb2c
Silvio Moioli <smoioli@suse.de>
2016-10-18 09:44:34 +0200

    Packer: non-KVM builders removed

--- packer/scripts/rhn_reg.sh
+++
@@ -1,1 +0,0 @@
-rhnreg_ks --username=<RHN or Satellite login> --password=<RHN or Satellite password> --force
--- packer/scripts/rhn_unreg.sh
+++
@@ -1,2 +0,0 @@
-# you still need to remove the system from your classic subscription management
-rm -f /etc/sysconfig/rhn/systemid
