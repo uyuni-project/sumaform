a439879026ce8ef758c916c6522b7ed34609b69d
Frantisek Kobzik <fkobzik@suse.de>
2016-10-19 11:33:35 +0200

    Don't fail when removing file fails

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -3,4 +3,4 @@ yum -y clean all
 
 # Remove traces of mac address from network configuration
 sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
-rm /etc/udev/rules.d/70-persistent-net.rules
+rm -f /etc/udev/rules.d/70-persistent-net.rules
