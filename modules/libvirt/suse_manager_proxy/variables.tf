f0212be9b7b4a537258a173ba1600591570d6b17
Silvio Moioli <smoioli@suse.de>
2016-11-08 16:42:40 +0100

    Make the for-development-only flag parametric at module level

--- modules/libvirt/suse_manager_proxy/variables.tf
+++ modules/libvirt/suse_manager_proxy/variables.tf
@@ -18,6 +18,11 @@ variable "server_configuration" {
   type = "map"
 }
 
+variable "for_development_only" {
+  description = "whether this host should be pre-configured with settings useful for development, but not necessarily safe in production"
+  default = true
+}
+
 variable "count"  {
   description = "number of hosts like this one"
   default = 1
