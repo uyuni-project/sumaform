f5b466c0191879c69c696f7b223a507e7dee88ca
Dario Maiocchi <MalloZup@users.noreply.github.com>
2016-11-11 18:23:27 +0100

    update main.tf with sles12sp2

--- modules/libvirt/base/main.tf
+++ modules/libvirt/base/main.tf
@@ -28,6 +28,13 @@ resource "libvirt_volume" "sles12sp1" {
   pool = "${var.pool}"
 }
 
+
+resource "libvirt_volume" "sles12sp2" {  
+  name = "${var.name_prefix}sles12sp2"
+  source = "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles12sp2.x86_64.qcow2"
+  pool = "${var.pool}"
+}
+
 output "configuration" {
   // HACK: work around https://github.com/hashicorp/terraform/issues/9549
   value = "${
