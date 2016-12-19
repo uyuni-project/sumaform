271df4c25ea51f8e1f186ebbda8a60f38f202900
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-19 16:01:33 +0100

    TYPO: Use ssh for hostname

--- modules/libvirt/control_node/main.tf
+++ modules/libvirt/control_node/main.tf
@@ -12,7 +12,7 @@ server: ${var.server_configuration["hostname"]}
 client: ${var.client_configuration["hostname"]}
 minion: ${var.minion_configuration["hostname"]}
 centos-minion: ${var.centos_configuration["hostname"]}
-ssh-minion: ${var.minion_configuration["hostname"]}
+ssh-minion: ${var.minionssh_configuration["hostname"]}
 role: control-node
 branch : ${var.branch}
 EOF
