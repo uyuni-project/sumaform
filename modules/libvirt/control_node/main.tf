dc26adf28c441c28ed0b33589927a7c4b10cb84f
Dario Maiocchi <MalloZup@users.noreply.github.com>
2016-12-22 13:45:47 +0100

    testsuite: add minion host for salt-ssh (#47)
    This adds one more minion host to the testsuite to conduct salt-ssh tests.

--- modules/libvirt/control_node/main.tf
+++ modules/libvirt/control_node/main.tf
@@ -12,8 +12,8 @@ server: ${var.server_configuration["hostname"]}
 client: ${var.client_configuration["hostname"]}
 minion: ${var.minion_configuration["hostname"]}
 centos-minion: ${var.centos_configuration["hostname"]}
+ssh-minion: ${var.minionssh_configuration["hostname"]}
 role: control-node
 branch : ${var.branch}
-
 EOF
 }
