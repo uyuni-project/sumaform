9dcd291d78577bcc886550473342aa51cf81338c
dmaiocchi <dmaiocchi@suse.com>
2017-03-01 17:51:31 +0100

    BUGFIX: fix phantomjs issue with secure websocket
    We need to have the suma-server certificate in order to
    visualize the button tristate on phantomjs.

--- salt/control-node/run-testsuite.sh
+++ salt/control-node/run-testsuite.sh
@@ -7,5 +7,11 @@ export CLIENT={{ grains.get("client") }}
 export MINION={{ grains.get("minion") }}
 export BROWSER=phantomjs
 
+# phantomjs need suma-server certificate for running secure websockets
+if [ ! -f /etc/pki/trust/anchors/$TESTHOST.cert ]; then
+  wget http://$TESTHOST/pub/RHN-ORG-TRUSTED-SSL-CERT -O /etc/pki/trust/anchors/$TESTHOST.cert
+  update-ca-certificates
+fi
+
 cd /root/spacewalk-testsuite-base
 rake
