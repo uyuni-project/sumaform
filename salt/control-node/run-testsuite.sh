81aa39c9ffa8f665c5fc751bf01e748cbbd2bea1
dmaiocchi <dmaiocchi@suse.com>
2017-03-20 22:40:57 +0100

    Remove browser variable.
    This variable is implicit, we use always phantomjs and
    do not support other browser support.

--- salt/control-node/run-testsuite.sh
+++ salt/control-node/run-testsuite.sh
@@ -5,7 +5,6 @@ export CENTOSMINION={{ grains.get("centos-minion") }}
 export TESTHOST={{ grains.get("server") }}
 export CLIENT={{ grains.get("client") }}
 export MINION={{ grains.get("minion") }}
-export BROWSER=phantomjs
 
 # phantomjs need suma-server certificate for running secure websockets
 if [ ! -f /etc/pki/trust/anchors/$TESTHOST.cert ]; then
