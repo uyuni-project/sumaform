3084c04fd417f13fcea50f497623ed1ed479e56b
Silvio Moioli <smoioli@suse.de>
2017-03-24 11:53:26 +0100

    AWS: add minion-swarm support

---
+++ modules/aws/minion-swarm/main.tf
@@ -0,0 +1,72 @@
+resource "aws_instance" "instance" {
+  ami = "${var.ami}"
+  instance_type = "t2.medium"
+  count = "${var.count}"
+  availability_zone = "${var.availability_zone}"
+  key_name = "${var.key_name}"
+  monitoring = "${var.monitoring}"
+  subnet_id = "${var.private_subnet_id}"
+  vpc_security_group_ids = ["${var.private_security_group_id}"]
+
+  root_block_device {
+    volume_size = 7
+    volume_type = "gp2"
+  }
+
+  tags {
+    Name = "${var.name_prefix}-${var.name}-${count.index}"
+  }
+}
+
+resource "null_resource" "host_salt_configuration" {
+  count = "${var.count}"
+
+  triggers {
+    instance_id = "${element(aws_instance.instance.*.id, count.index)}"
+  }
+
+  connection {
+    host = "${element(aws_instance.instance.*.private_dns, count.index)}"
+    private_key = "${file(var.key_file)}"
+    bastion_host = "${var.package_mirror_public_name}"
+  }
+
+  provisioner "file" {
+    source = "salt"
+    destination = "/srv/salt"
+  }
+
+  provisioner "file" {
+    content = <<EOF
+
+hostname: ${replace("${element(aws_instance.instance.*.private_dns, count.index)}", ".${var.region}.compute.internal", "")}
+domain: ${var.region}.compute.internal
+use-avahi: False
+package-mirror: ${var.package_mirror_private_name}
+version: 3-stable
+role: minion-swarm
+server: ${var.server}
+minion-count: 100
+start-delay: 3
+swap_file_size: 3000
+timezone: ${var.timezone}
+authorized-keys: null
+additional_repos: {${join(", ", formatlist("'%s': '%s'", keys(var.additional_repos), values(var.additional_repos)))}}
+additional_packages: [${join(", ", formatlist("'%s'", var.additional_packages))}]
+
+EOF
+
+    destination = "/etc/salt/grains"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "salt-call --force-color --local --output=quiet state.sls default,terraform-resource",
+      "salt-call --force-color --local state.highstate"
+    ]
+  }
+}
+
+output "private_names" {
+  value = ["${aws_instance.instance.*.private_dns}"]
+}
