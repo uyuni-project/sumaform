6e1ceef6e2f682dd21b6e764ecd48050769a5879
Michele Bologna <michele.bologna@suse.com>
2017-10-17 16:56:10 +0200

    Feat: HEAD runs on SP3

--- modules/libvirt/suse_manager/main.tf
+++ modules/libvirt/suse_manager/main.tf
@@ -6,7 +6,7 @@ variable "images" {
     "3.0-nightly" = "sles12sp1"
     "3.1-released" = "sles12sp2"
     "3.1-nightly" = "sles12sp2"
-    "head" = "sles12sp2"
+    "head" = "sles12sp3"
     "test" = "sles12sp2"
   }
 }
