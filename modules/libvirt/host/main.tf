a48fe9464a690ed95c38f9a13a79085f596a8456
Silvio Moioli <smoioli@suse.de>
2017-10-31 09:45:09 +0100

    Bugfix: use correct command for minimal setup

--- modules/libvirt/host/main.tf
+++ modules/libvirt/host/main.tf
@@ -64,7 +64,7 @@ EOF
 
   provisioner "remote-exec" {
     inline = [
-      "salt-call --local --file-root=/root/salt/ --output=quiet state.sls_id minimal_package_update default",
+      "salt-call --local --file-root=/root/salt/ --output=quiet state.sls default",
       "salt-call --local --file-root=/root/salt/ --log-level=info --force-color state.highstate"
     ]
   }
