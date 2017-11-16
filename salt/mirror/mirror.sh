e48b7b12c08b9bae07dfe52e97f231ac370e7c75
Michele Bologna <michele.bologna@gmail.com>
2017-11-16 23:04:35 +0100

    Fix: append stdout and redirect stderr to logfile

--- salt/mirror/mirror.sh
+++ salt/mirror/mirror.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 cd /srv/mirror
-minima -c /root/minima.yaml sync >/var/log/minima.log
+minima -c /root/minima.yaml sync >>/var/log/minima.log 2>&1
 /root/refresh_scc_data.py {{ grains.get("cc_username") }}:{{ grains.get("cc_password") }}
 
 chmod -R 777 .
