1c52f8b92b86d44e63826ee97d10580ce176b887
Silvio Moioli <smoioli@suse.de>
2018-02-13 12:07:29 +0100

    Bugfix: use EPEL repo properly

--- packer/scripts/base.sh
+++ packer/scripts/base.sh
@@ -6,3 +6,7 @@ yum -y install ca-certificates
 
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
+
+# Install the EPEL repo in order to install nss-mdns
+yum -y install epel-release
+yum -y install nss-mdns
