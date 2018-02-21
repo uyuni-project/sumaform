fbbf0a124d92bcef2335fe43f569b9511b1d4d52
Silvio Moioli <smoioli@suse.de>
2018-02-21 11:54:56 +0100

    Assume locust runs on localhost:80

--- salt/locust/run-locust.py
+++ salt/locust/run-locust.py
@@ -11,19 +11,11 @@ parser.add_argument('-r', '--hatch-rate', action='store', dest='hatch_rate',
                     default=100, type=int,
                     help='The rate per second in which clients are spawned')
 inputUser = parser.parse_args()
-print('locust users: ', inputUser.clients)
-print('locust hatch rate: ', inputUser.hatch_rate)
+
 LocustPayload = {
     'locust_count': inputUser.clients,
     'hatch_rate': inputUser.hatch_rate
 }
 
-# start locust webui (needed for locust_export statistics)
-# We assume implicity that locust and locust_exporter run in the same server
-locustLocalhost = 'http://localhost:8089/swarm'
-
-# TODO: add locust file/s to be executed
-return_code = subprocess.call("locust -f ", shell=True)  
-
-res = requests.post(locustLocalhost, data=LocustPayload)
+res = requests.post('http://localhost/swarm', data=LocustPayload)
 print(res.json())
