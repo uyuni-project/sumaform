523cec2612a3a4e1f91467fd0748cdd684862a8d
Silvio Moioli <smoioli@suse.de>
2018-02-21 11:54:36 +0100

    pep8 fixes

--- salt/locust/run-locust.py
+++ salt/locust/run-locust.py
@@ -3,11 +3,12 @@
 import requests
 import argparse
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--clients' action='store', dest='clients', default=200, type=int,
+parser.add_argument('-c', '--clients', action='store', dest='clients',
+                    default=200, type=int,
                     help='Number of concurrent clients')
-parser.add_argument('-r', '--hatch-rate', action='store', dest='hatch_rate', default=100, type=int,
+parser.add_argument('-r', '--hatch-rate', action='store', dest='hatch_rate',
+                    default=100, type=int,
                     help='The rate per second in which clients are spawned')
 inputUser = parser.parse_args()
 print('locust users: ', inputUser.clients)
