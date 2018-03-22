940b6e27be686b7097b2ef3b25caec1fab6d4970
MalloZup <dmaiocchi@suse.com>
2018-03-22 08:49:26 +0100

    add set -e for having in CI the exact retcode

--- salt/controller/run-testsuite.sh
+++ salt/controller/run-testsuite.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 cd /root/spacewalk/testsuite
 
 if [ -z "$1" ]; then
    rake
 fi
 # this to run cucumber features in parallel
-if [ $1 = "parallel" ]; then
+if [[ $1 = "parallel" ]]; then
    rake core
    rake secondary_parallel
    rake post_run
