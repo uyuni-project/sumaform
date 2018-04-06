a5896ca091448e78fbd535957e20b8bb62a5db48
Silvio Moioli <smoioli@suse.de>
2018-04-06 12:04:06 +0200

    Bugfix: shut Salt up when errors are expected

--- modules/libvirt/host/main.tf
+++ modules/libvirt/host/main.tf
@@ -70,7 +70,7 @@ EOF
 
   provisioner "remote-exec" {
     inline = [
-      "salt-call --local --file-root=/root/salt/ --output=quiet state.sls default",
+      "salt-call --local --file-root=/root/salt/ --log-level=quiet --output=quiet state.sls default",
       "salt-call --local --file-root=/root/salt/ --log-level=info --retcode-passthrough --force-color state.highstate"
     ]
   }
