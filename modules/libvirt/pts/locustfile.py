22626e36758827f5aeec2d0a8974882d81b97dba
Silvio Moioli <smoioli@suse.de>
2018-04-24 10:22:24 +0200

    PTS: allow big and small package downloads
    Big package downloads are disabled by default

--- modules/libvirt/pts/locustfile.py
+++ modules/libvirt/pts/locustfile.py
@@ -8,21 +8,23 @@ class UserBehavior(TaskSet):
         """ on_start is called when a Locust start before any task is scheduled """
         # don't verify ssl certs
         self.client.verify = False
-        self.download_path = self.get_download_path()
+        self.small_package_path = self.get_download_path("cloned-2017-q4-sles12-sp3-updates-x86_64", "at-3.1.14-8.3.1.x86_64.rpm")
+        self.big_package_path = self.get_download_path("cloned-2017-q4-sles12-sp3-updates-x86_64", "kernel-default-4.4.103-6.33.1.x86_64.rpm")
 
-    def get_download_path(self):
-        channel_name = 'cloned-2017-q4-sles12-sp3-updates-x86_64'
-        package_name = 'at-3.1.14-8.3.1.x86_64.rpm'
-
+    def get_download_path(self, channel_name, package_name):
         response = self.client.get("/pub/pillar_data/pillar_evil-minions-0.yml")
         json_pillar = yaml.load(response.content)
         validation_token = json_pillar['channels'][channel_name]['token']
 
         return "/rhn/manager/download/" + channel_name + "/getPackage/" + package_name + "?" + validation_token;
 
-    @task(1)
-    def download_package(self):
-        self.client.get(self.download_path)
+    @task(100)
+    def download_small_package(self):
+        self.client.get(self.small_package_path)
+
+    @task(0)
+    def download_big_package(self):
+        self.client.get(self.big_package_path)
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
