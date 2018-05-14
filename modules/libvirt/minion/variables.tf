538b53fab64fa54fdee3842e03e47255aeb0c4c5
MalloZup <dmaiocchi@suse.com>
2018-05-14 10:19:57 +0200

    move to sles15rc4

--- modules/libvirt/minion/variables.tf
+++ modules/libvirt/minion/variables.tf
@@ -77,7 +77,7 @@ variable "gpg_keys" {
 // Provider-specific variables
 
 variable "image" {
-  description = "One of: sles11sp4, sles12, sles12sp1, sles12sp2, sles12sp3, sles15rc3, centos7"
+  description = "One of: sles11sp4, sles12, sles12sp1, sles12sp2, sles12sp3, sles15rc4, centos7"
   type = "string"
 }
 
