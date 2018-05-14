7e46df6d1ec41d8260892fc77cce47323e6f48ba
MalloZup <dmaiocchi@suse.com>
2018-05-14 16:50:42 +0200

    add sles-es7.4 packer img

---
+++ packer/scripts/base-es.sh
@@ -0,0 +1,10 @@
+rpm --import http://nu.novell.com/repo/$RCE/RES7-SUSE-Manager-Tools/x86_64/repodata/repomd.xml.key
+
+yum -y update
+yum -y install wget curl openssh-server
+
+# Install root certificates
+yum -y install ca-certificates
+
+# Make ssh faster by not waiting on DNS
+echo "UseDNS no" >> /etc/ssh/sshd_config
