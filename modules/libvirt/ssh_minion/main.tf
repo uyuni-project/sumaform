211278f8d42a7f17c1a9b0232b1f11d1f0d950aa
Eric Bischoff <ebischoff@suse.com>
2018-05-23 12:26:49 +0200

    Adding new role: SSH minion

---
+++ modules/libvirt/ssh_minion/main.tf
@@ -0,0 +1,34 @@
+module "ssh_minion" {
+  source = "../host"
+
+  base_configuration = "${var.base_configuration}"
+  name = "${var.name}"
+  count = "${var.count}"
+  use_released_updates = "${var.use_released_updates}"
+  use_unreleased_updates = "${var.use_unreleased_updates}"
+  additional_repos = "${var.additional_repos}"
+  additional_packages = "${var.additional_packages}"
+  gpg_keys = "${var.gpg_keys}"
+  ssh_key_path = "${var.ssh_key_path}"
+  grains = <<EOF
+
+version: ${var.version}
+mirror: ${var.base_configuration["mirror"]}
+role: ssh_minion
+testsuite: ${var.base_configuration["testsuite"]}
+evil_minions_dump: ${var.evil_minions_dump}
+apparmor: ${var.apparmor}
+
+EOF
+
+  // Provider-specific variables
+  image = "${var.image}"
+  memory = "${var.memory}"
+  vcpu = "${var.vcpu}"
+  running = "${var.running}"
+  mac = "${var.mac}"
+}
+
+output "configuration" {
+  value = "${module.ssh_minion.configuration}"
+}
---
+++ modules/openstack/ssh_minion/variables.tf
@@ -0,0 +1,72 @@
+variable "base_configuration" {
+  description = "use ${module.base.configuration}, see the main.tf example file"
+  type = "map"
+}
+
+variable "name" {
+  description = "hostname, without the domain part"
+  type = "string"
+}
+
+variable "evil_minions_dump" {
+  description = "whether this minion should create a dump file to be used for evil-minions"
+  default = false
+}
+
+variable "use_released_updates" {
+  description = "Apply all updates from SUSE Linux Enterprise repos"
+  default = false
+}
+
+variable "use_unreleased_updates" {
+  description = "Apply all updates from SUSE Linux Enterprise unreleased (Test) repos"
+  default = false
+}
+
+variable "apparmor" {
+  description = "whether AppArmor access control should be installed"
+  default = false
+}
+
+variable "additional_repos" {
+  description = "extra repositories used for installation {label = url}"
+  default = {}
+}
+
+variable "additional_packages" {
+  description = "extra packages which should be installed"
+  default = []
+}
+
+variable "count"  {
+  description = "number of hosts like this one"
+  default = 1
+}
+
+variable "ssh_key_path" {
+  description = "path of additional pub ssh key you want to use to access VMs, see README_ADVANCED.md"
+  default = "/dev/null"
+  # HACK: "" cannot be used as a default because of https://github.com/hashicorp/hil/issues/50
+}
+
+variable "gpg_keys" {
+  description = "salt/ relative paths of gpg keys that you want to add to your VMs, see README_ADVANCED.md"
+  default = []
+}
+
+// Provider-specific variables
+
+variable "image" {
+  description = "One of: sles11sp4, sles12, sles12sp1, sles12sp2, sles12sp3, sles15rc4, centos7"
+  type = "string"
+}
+
+variable "flavor" {
+  description = "OpenStack flavor"
+  default = "m1.small"
+}
+
+variable "floating_ips" {
+  description = "List of floating IP IDs to associate"
+  default = []
+}
