7ab5b2900688262a5dbd61fb4c82612d77105348
MalloZup <dmaiocchi@suse.com>
2018-06-05 15:32:42 +0200

    fix avahi for sles-es7
    and mdns.allow file and pkg

--- packer/scripts/base-es.sh
+++ packer/scripts/base-es.sh
@@ -4,6 +4,8 @@ yum -y install wget curl openssh-server
 
 # Install root certificates
 yum -y install ca-certificates
-
+# install epel7 for l nss-mdns
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install nss-mdns
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
