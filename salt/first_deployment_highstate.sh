951297b5b4316dc1a51836f526f03f7bc8d93e74
Michael Calmer <mc@suse.de>
2018-06-07 11:33:36 +0200

    ignore errors on first state call but exit when highstate failed

--- salt/first_deployment_highstate.sh
+++ salt/first_deployment_highstate.sh
@@ -3,7 +3,7 @@
 # Applies the "minimal" state in isolation to set the hostname and update Salt itself
 # then applies the highstate
 
-salt-call --local --file-root=/root/salt/ --log-level=quiet --output=quiet state.sls default.minimal
-salt-call --local --file-root=/root/salt/ --log-level=info --retcode-passthrough --force-color state.highstate
+salt-call --local --file-root=/root/salt/ --log-level=quiet --output=quiet state.sls default.minimal ||:
+salt-call --local --file-root=/root/salt/ --log-level=info --retcode-passthrough --force-color state.highstate || exit 1
 
 chmod +x /root/salt/highstate.sh
