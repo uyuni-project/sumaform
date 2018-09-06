2b02e4fb0fc08e3efde3d35ba6736f2b4f86d607
Eric Bischoff <ebischoff@suse.com>
2018-09-06 14:55:58 +0200

    Remove Ethernet type from ifcfg-ethX as we might use virtio

--- packer/scripts/base.sh
+++ packer/scripts/base.sh
@@ -7,6 +7,11 @@ yum -y install ca-certificates
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
 
+# We might use virtio interfaces, undeclare Ethernet
+for card in eth0 eth1; do
+  sed -i 's/^TYPE="Ethernet"/# &/' /etc/sysconfig/network-scripts/ifcfg-$card
+done
+
 # Install the EPEL repo in order to install nss-mdns
 yum -y install epel-release
 yum -y install nss-mdns
