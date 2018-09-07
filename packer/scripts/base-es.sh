ca17f24a5b8d6ccdc3ec9aedfeacdfd63d880433
Eric Bischoff <ebischoff@suse.com>
2018-09-07 20:56:42 +0200

    Cleanup packer code

--- packer/scripts/base-es.sh
+++ packer/scripts/base-es.sh
@@ -1,11 +1,15 @@
 rpm --import 'http://nu.novell.com/repo/$RCE/RES7-SUSE-Manager-Tools/x86_64/repodata/repomd.xml.key'
+
+# Install basic packages
 yum -y update
 yum -y install wget curl openssh-server
 
 # Install root certificates
 yum -y install ca-certificates
-# install multicast DNS plugin for nss from epel
+
+# Install multicast DNS plugin for nss from epel
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install nss-mdns
-# Make ssh faster by not waiting on DNS
+
+# Make SSH faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
