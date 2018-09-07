652045ceff71e0d5888d98d8c5f31dfd6e0b6291
Eric Bischoff <ebischoff@suse.com>
2018-09-07 20:56:42 +0200

    Cleanup packer code

--- packer/scripts/base.sh
+++ packer/scripts/base.sh
@@ -1,17 +1,16 @@
+# Install basic packages
 yum -y update
 yum -y install wget curl openssh-server
 
 # Install root certificates
 yum -y install ca-certificates
 
-# Make ssh faster by not waiting on DNS
+# Install multicast DNS plugin for nss from epel
+yum -y install epel-release
+yum -y install nss-mdns
+
+# Make SSH faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
 
 # We might use virtio interfaces, undeclare Ethernet
-for card in eth0 eth1; do
-  sed -i 's/^TYPE="Ethernet"/# &/' /etc/sysconfig/network-scripts/ifcfg-$card
-done
-
-# Install the EPEL repo in order to install nss-mdns
-yum -y install epel-release
-yum -y install nss-mdns
+sed -i 's/^TYPE="Ethernet"/# &/' /etc/sysconfig/network-scripts/ifcfg-eth0
