63c3090addd992b935f083e1f19a332563071622
Eric Bischoff <ebischoff@suse.com>
2018-09-08 14:37:27 +0200

    Remove Ethernet type from ifcfg-ethX as we might use virtio

--- packer/scripts/base-es.sh
+++ packer/scripts/base-es.sh
@@ -13,3 +13,6 @@ yum -y install nss-mdns
 
 # Make SSH faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
+
+# We might use virtio interfaces, undeclare Ethernet
+sed -i 's/^TYPE="Ethernet"/# &/' /etc/sysconfig/network-scripts/ifcfg-eth0
