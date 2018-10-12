77bd416b8a4aa76febccb251a7765c58f5970caa
Eric Bischoff <ebischoff@suse.com>
2018-10-12 23:04:53 +0200

    Fix error on CentOS 7
    "Device eth0 does not seem to be present, delaying initialisation"

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -4,3 +4,6 @@ yum -y clean all
 # Remove traces of mac address from network configuration
 sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
 rm -f /etc/udev/rules.d/70-persistent-net.rules
+
+# Since there is no MAC address anymore, we need to identify the card
+echo 'DEVICE="eth0"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
