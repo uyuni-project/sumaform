678b0831fe39dd8a268ddd56e4e6630425f77607
Silvio Moioli <smoioli@suse.de>
2018-11-16 13:38:12 +0100

    Remove Packer files
    Non-OpenBuildService images are now tracked and built in:
    
    https://github.com/moio/sumaform-images

--- packer/scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-yum -y erase gtk2 libX11 hicolor-icon-theme freetype bitstream-vera-fonts
-yum -y clean all
-
-# Remove traces of mac address from network configuration
-sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
-rm -f /etc/udev/rules.d/70-persistent-net.rules
-
-# Since there is no MAC address anymore, we need to identify the card
-echo 'DEVICE="eth0"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
