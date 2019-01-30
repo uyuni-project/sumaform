7e4c808e98c83b6dc2ece8a36933fd08ea013e2b
Michele Bologna <michele.bologna@suse.com>
2019-01-30 14:58:32 +0100

    Feat: change default OS to install Uyuni Proxy (OpenStack)

--- modules/openstack/suse_manager_proxy/main.tf
+++ modules/openstack/suse_manager_proxy/main.tf
@@ -7,7 +7,7 @@ variable "images" {
     "3.2-released" = "sles12sp4"
     "3.2-nightly" = "sles12sp4"
     "head" = "sles15sp1"
-    "uyuni-released" = "opensuse423"
+    "uyuni-released" = "opensuse150"
   }
 }
 
