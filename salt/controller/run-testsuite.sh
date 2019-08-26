9bd23b2542fa7d07a40414e4e46d3d28325dd915
Oscar Barrios <obarrios@suse.com>
2019-08-26 11:05:05 +0200

    Update run-testsuite to use the new rake tasks

--- salt/controller/run-testsuite.sh
+++ salt/controller/run-testsuite.sh
@@ -9,7 +9,9 @@ if [ -z "$1" ]; then
 fi
 # this to run cucumber features in parallel
 if [[ $1 = "parallel" ]]; then
-   rake core
-   rake secondary_parallel
-   rake post_run
+   rake cucumber:core
+   rake parallel:init_clients
+   rake cucumber:secondary
+   rake parallel:secondary_parallelizable
+   rake cucumber:finishing
 fi
