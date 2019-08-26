45e41a5e32c7d04549c314a3505e98d7d0ca5613
Oscar Barrios <obarrios@suse.com>
2019-08-26 15:10:34 +0200

    Adding new options to run-testsuite

--- salt/controller/run-testsuite.sh
+++ salt/controller/run-testsuite.sh
@@ -5,11 +5,33 @@ set -e
 cd /root/spacewalk/testsuite
 
 if [ -z "$1" ]; then
-   rake
+   rake cucumber:core
+   rake cucumber:reposync
+   rake cucumber:init_clients
+   rake cucumber:secondary
+   rake cucumber:secondary_parallelizable
+   rake cucumber:finishing
 fi
+
+# this to run cucumber features in refhost
+if [[ $1 = "refhost" ]]; then
+   rake cucumber:refhost
+fi
+
+# this to run cucumber features in virtualization
+if [[ $1 = "virtualization" ]]; then
+   rake cucumber:virtualization
+fi
+
+# this to run cucumber features in sle-updates
+if [[ $1 = "sle-updates" ]]; then
+   rake cucumber:sle-updates
+fi
+
 # this to run cucumber features in parallel
 if [[ $1 = "parallel" ]]; then
    rake cucumber:core
+   rake cucumber:reposync
    rake parallel:init_clients
    rake cucumber:secondary
    rake parallel:secondary_parallelizable
