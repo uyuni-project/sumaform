46a0e8da1429cbec134eed7681ca548f7315ed52
Michael Calmer <mc@suse.de>
2019-09-26 17:55:42 +0200

    mirror images

--- salt/mirror/mirror.sh
+++ salt/mirror/mirror.sh
@@ -8,6 +8,20 @@ minima -c /root/minima.yaml sync 2>&1 | tee /var/log/minima.log
 
 apt-mirror
 
+# will mirror them in /srv/mirror under the same path as the URL
+wget --mirror --no-host-directories "https://github.com/moio/sumaform-images/releases/download/4.3.0/centos7.qcow2"
+wget --mirror --no-host-directories "https://download.opensuse.org/repositories/systemsmanagement:/sumaform:/images:/libvirt/images/opensuse150.x86_64.qcow2"
+wget --mirror --no-host-directories "https://download.opensuse.org/repositories/systemsmanagement:/sumaform:/images:/libvirt/images/opensuse151.x86_64.qcow2"
+wget --mirror --no-host-directories "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles15.x86_64.qcow2"
+wget --mirror --no-host-directories "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles15sp1.x86_64.qcow2"
+wget --mirror --no-host-directories "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles11sp4.x86_64.qcow2"
+wget --mirror --no-host-directories "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles12.x86_64.qcow2"
+wget --mirror --no-host-directories "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles12sp1.x86_64.qcow2"
+wget --mirror --no-host-directories "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles12sp2.x86_64.qcow2"
+wget --mirror --no-host-directories "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles12sp3.x86_64.qcow2"
+wget --mirror --no-host-directories "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles12sp4.x86_64.qcow2"
+wget --mirror --no-host-directories "https://github.com/moio/sumaform-images/releases/download/4.3.0/ubuntu1804.qcow2"
+
 jdupes --linkhard -r -s /srv/mirror/
 
 chmod -R 777 .
