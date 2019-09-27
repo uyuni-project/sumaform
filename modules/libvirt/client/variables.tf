f21bf78951d111d217629a403aae9f5d1796d435
Silvio Moioli <moio@suse.com>
2019-09-27 11:10:16 +0200

    0.12: client module translation

--- modules/libvirt/client/variables.tf
+++ modules/libvirt/client/variables.tf
@@ -1,74 +1,73 @@
 variable "base_configuration" {
   description = "use module.base.configuration, see the main.tf example file"
-  type = "map"
+  type        = map(string)
 }
 
 variable "name" {
   description = "hostname, without the domain part"
-  type = "string"
+  type        = string
 }
 
 variable "product_version" {
   description = "A valid SUSE Manager version (eg. 3.2-nightly, head) see README_ADVANCED.md"
-  default = "released"
+  default     = "released"
 }
 
 variable "server_configuration" {
   description = "use module.<SERVER_NAME>.configuration, see the main.tf example file"
-  type = "map"
+  type        = map(string)
 }
 
 variable "auto_register" {
   description = "whether this client should be automatically registered to SUSE Manager upon deployment"
-  default = true
+  default     = true
 }
 
 variable "use_os_released_updates" {
   description = "Apply all updates from SUSE Linux Enterprise repos"
-  default = false
+  default     = false
 }
 
 variable "use_os_unreleased_updates" {
   description = "Apply all updates from SUSE Linux Enterprise unreleased (Test) repos"
-  default = false
+  default     = false
 }
 
 variable "additional_repos" {
   description = "extra repositories in the form {label = url}, see README_ADVANCED.md"
-  default = {}
+  default     = {}
 }
 
 variable "additional_packages" {
   description = "extra packages to install, see README_ADVANCED.md"
-  default = []
+  default     = []
 }
 
-variable "quantity"  {
+variable "quantity" {
   description = "number of hosts like this one"
-  default = 1
+  default     = 1
 }
 
 variable "swap_file_size" {
   description = "Swap file size in MiB, or 0 for none"
-  default = 0
+  default     = 0
 }
 
 variable "ssh_key_path" {
   description = "path of additional pub ssh key you want to use to access VMs, see README_ADVANCED.md"
-  default = "/dev/null"
+  default     = "/dev/null"
   # HACK: "" cannot be used as a default because of https://github.com/hashicorp/hil/issues/50
 }
 
 variable "gpg_keys" {
   description = "salt/ relative paths of gpg keys that you want to add to your VMs, see README_ADVANCED.md"
-  default = []
+  default     = []
 }
 
 variable "ipv6" {
   description = "IPv6 tuning: enable it, accept the RAs"
-  type = "map"
   default = {
-    enable = true
+    enable    = true
     accept_ra = true
   }
 }
@@ -77,30 +76,30 @@ variable "ipv6" {
 
 variable "image" {
   description = "One of: sles11sp4, sles12, sles12sp1, sles12sp2, sles12sp3, sles12sp4, sles15, centos7"
-  type = "string"
+  type        = string
 }
 
 variable "memory" {
   description = "RAM memory in MiB"
-  default = 1024
+  default     = 1024
 }
 
 variable "vcpu" {
   description = "Number of virtual CPUs"
-  default = 1
+  default     = 1
 }
 
 variable "running" {
   description = "Whether this host should be turned on or off"
-  default = true
+  default     = true
 }
 
 variable "mac" {
   description = "a MAC address in the form AA:BB:CC:11:22:22"
-  default = ""
+  default     = ""
 }
 
 variable "cpu_model" {
   description = "Define what CPU model the guest is getting (host-model, host-passthrough or the default)."
-  default = ""
+  default     = ""
 }
