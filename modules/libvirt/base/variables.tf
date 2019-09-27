f903ccbbbd2ecc61514cd12b7484b1edd3d4aa83
Silvio Moioli <moio@suse.com>
2019-09-27 10:08:07 +0200

    0.12: base module translation

--- modules/libvirt/base/variables.tf
+++ modules/libvirt/base/variables.tf
@@ -1,82 +1,81 @@
 variable "cc_username" {
   description = "username for the Customer Center"
-  type = "string"
+  type        = string
 }
 
 variable "cc_password" {
   description = "password for the Customer Center"
-  type = "string"
+  type        = string
 }
 
 variable "timezone" {
   description = "Timezone setting for all VMs"
-  default = "Europe/Berlin"
+  default     = "Europe/Berlin"
 }
 
 variable "ssh_key_path" {
   description = "path of pub ssh key you want to use to access VMs, see libvirt/README.md"
-  default = "~/.ssh/id_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "mirror" {
   description = "hostname of the mirror host or leave the default for no mirror"
-  default = "null"
+  default     = "null"
 }
 
 variable "use_mirror_images" {
   description = "use true download images from a mirror host"
-  default = false
+  default     = false
 }
 
 variable "use_avahi" {
   description = "use false only if you use bridged networking with static MACs and an external DHCP"
-  default = true
+  default     = true
 }
 
 variable "domain" {
   description = "hostname's domain"
-  default = "tf.local"
+  default     = "tf.local"
 }
 
 variable "name_prefix" {
   description = "a prefix for all names of objects to avoid collisions. E.g. moio-"
-  default = ""
+  default     = ""
 }
 
 variable "use_shared_resources" {
   description = "use true to avoid deploying images, mirrors and other shared infrastructure resources"
-  default = false
+  default     = false
 }
 
 variable "testsuite" {
   description = "true to enable specific setup for the integration testsuite"
-  default = false
+  default     = false
 }
 
 variable "additional_network" {
   description = "additional network in CIDR notation, leave empty if none"
-  default = ""
+  default     = ""
 }
 
 // Provider-specific variables
 
 variable "pool" {
   description = "libvirt storage pool name for VM disks"
-  default = "default"
+  default     = "default"
 }
 
 variable "network_name" {
   description = "libvirt NAT network name for VMs, use empty string for bridged networking"
-  default = "default"
+  default     = "default"
 }
 
 variable "bridge" {
   description = "a bridge device name available on the libvirt host, leave default for NAT"
-  default = ""
+  default     = ""
 }
 
 variable "images" {
   description = "list of images to be uploaded to the libvirt host, leave default for all"
-  default = ["centos7", "opensuse150", "opensuse151", "sles15", "sles15sp1", "sles15sp2", "sles11sp4", "sles12", "sles12sp1", "sles12sp2", "sles12sp3", "sles12sp4", "ubuntu1804"]
-  type = "list"
+  default     = ["centos7", "opensuse150", "opensuse151", "sles15", "sles15sp1", "sles15sp2", "sles11sp4", "sles12", "sles12sp1", "sles12sp2", "sles12sp3", "sles12sp4", "ubuntu1804"]
 }
