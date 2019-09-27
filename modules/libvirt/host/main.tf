bdae90444d5f88ec5363e9325f60af3631a9f7fa
Silvio Moioli <moio@suse.com>
2019-09-27 15:31:51 +0200

    0.12: host module translation

--- modules/libvirt/host/main.tf
+++ modules/libvirt/host/main.tf
@@ -1,7 +1,3 @@
-terraform {
-    required_version = "~> 0.12.9"
-}
-
 // Names are calculated as follows:
 // ${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index  + 1}" : ""}
 // This means:
@@ -9,50 +5,73 @@ terraform {
 //   name_prefix + name + "-" + index (if quantity > 1)
 
 resource "libvirt_volume" "main_disk" {
-  name = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index  + 1}" : ""}-main-disk"
+  name             = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index + 1}" : ""}-main-disk"
   base_volume_name = "${var.base_configuration["use_shared_resources"] ? "" : var.base_configuration["name_prefix"]}${var.image}"
-  pool = "${var.base_configuration["pool"]}"
-  count = "${var.quantity}"
+  pool             = var.base_configuration["pool"]
+  count            = var.quantity
 }
 
 resource "libvirt_domain" "domain" {
-  name = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index  + 1}" : ""}"
-  memory = "${var.memory}"
-  vcpu = "${var.vcpu}"
-  running = "${var.running}"
-  count = "${var.quantity}"
+  name       = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index + 1}" : ""}"
+  memory     = var.memory
+  vcpu       = var.vcpu
+  running    = var.running
+  count      = var.quantity
   qemu_agent = true
 
   // copy host CPU model to guest to get the vmx flag if present
-  cpu {
-    mode = "${var.cpu_model != "" ? "${var.cpu_model}" : "custom"}"
+  cpu = {
+    mode = var.cpu_model != "" ? var.cpu_model : "custom"
   }
 
   // base disk + additional disks if any
-  disk = ["${concat(
-    list(
-      map("volume_id", "${element(libvirt_volume.main_disk.*.id, count.index)}")
-    ),
-    var.additional_disk
-  )}"]
+  dynamic "disk" {
+    for_each = concat(
+      [
+        {
+          "volume_id" = element(libvirt_volume.main_disk.*.id, count.index)
+        },
+      ],
+      var.additional_disk,
+    )
+    content {
+      volume_id = disk.value.volume_id
+    }
+  }
 
-  network_interface = ["${slice(
-    list(
-      map("wait_for_lease", true,
-          "network_name", var.base_configuration["network_name"],
-          "bridge", var.base_configuration["bridge"],
-          "mac", var.mac
-         ),
-      map("wait_for_lease", false,
-          "network_id", var.base_configuration["additional_network_id"]
-         )
-    ),
-    var.connect_to_base_network ? 0 : 1,
-    ((var.base_configuration["additional_network"] != "") && var.connect_to_additional_network) ? 2 : 1
-  )}"]
+  dynamic "network_interface" {
+    for_each = slice(
+      [
+        {
+          "wait_for_lease" = true
+          "network_name"   = var.base_configuration["network_name"]
+          "network_id"     = null
+          "bridge"         = var.base_configuration["bridge"]
+          "mac"            = var.mac
+        },
+        {
+          "wait_for_lease" = false
+          "network_name"   = null
+          "network_id"     = var.base_configuration["additional_network_id"]
+          "bridge"         = null
+          "mac"            = null
+        },
+      ],
+      var.connect_to_base_network ? 0 : 1,
+      var.base_configuration["additional_network"] != "" && var.connect_to_additional_network ? 2 : 1,
+    )
+    content {
+      wait_for_lease = network_interface.value.wait_for_lease
+      network_id     = network_interface.value.network_id
+      network_name   = network_interface.value.network_name
+      bridge         = network_interface.value.bridge
+      mac            = network_interface.value.mac
+    }
+  }
 
   connection {
-    user = "root"
+    host     = self.network_interface[0].addresses[0]
+    user     = "root"
     password = "linux"
   }
 
@@ -60,29 +79,33 @@ resource "libvirt_domain" "domain" {
     type        = "pty"
     target_port = "0"
     target_type = "serial"
+    source_host = null
+    source_service = null
   }
 
   console {
-      type        = "pty"
-      target_type = "virtio"
-      target_port = "1"
+    type        = "pty"
+    target_port = "1"
+    target_type = "virtio"
+    source_host = null
+    source_service = null
   }
 
   graphics {
-    type = "spice"
+    type        = "spice"
     listen_type = "address"
-    autoport = true
+    autoport    = true
   }
 
   provisioner "file" {
-    source = "salt"
+    source      = "salt"
     destination = "/root"
   }
 
   provisioner "file" {
     content = <<EOF
 
-hostname: ${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index  + 1}" : ""}
+hostname: ${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index + 1}" : ""}
 domain: ${var.base_configuration["domain"]}
 use_avahi: ${var.base_configuration["use_avahi"]}
 additional_network: ${var.base_configuration["additional_network"]}
@@ -91,9 +114,23 @@ testsuite: ${var.base_configuration["testsuite"]}
 roles: [${join(",", var.roles)}]
 use_os_released_updates: ${var.use_os_released_updates}
 use_os_unreleased_updates: ${var.use_os_unreleased_updates}
-additional_repos: {${join(", ", formatlist("'%s': '%s'", keys(var.additional_repos), values(var.additional_repos)))}}
+additional_repos: {${join(
+    ", ",
+    formatlist(
+      "'%s': '%s'",
+      keys(var.additional_repos),
+      values(var.additional_repos),
+    ),
+    )}}
 additional_repos_only: ${var.additional_repos_only}
-additional_certs: {${join(", ", formatlist("'%s': '%s'", keys(var.additional_certs), values(var.additional_certs)))}}
+additional_certs: {${join(
+    ", ",
+    formatlist(
+      "'%s': '%s'",
+      keys(var.additional_certs),
+      values(var.additional_certs),
+    ),
+    )}}
 additional_packages: [${join(", ", formatlist("'%s'", var.additional_packages))}]
 swap_file_size: ${var.swap_file_size}
 authorized_keys: [${trimspace(file(var.base_configuration["ssh_key_path"]))},${trimspace(file(var.ssh_key_path))}]
@@ -101,28 +138,32 @@ gpg_keys: [${join(", ", formatlist("'%s'", var.gpg_keys))}]
 connect_to_base_network: ${var.connect_to_base_network}
 connect_to_additional_network: ${var.connect_to_additional_network}
 reset_ids: true
-ipv6: {${join(", ", formatlist("'%s': %s", keys(var.ipv6), values(var.ipv6)))}}
+ipv6: {${join(
+    ", ",
+    formatlist("'%s': %s", keys(var.ipv6), values(var.ipv6)),
+)}}
 ${var.grains}
 
 EOF
 
-    destination = "/etc/salt/grains"
-  }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sh /root/salt/first_deployment_highstate.sh"
-    ]
-  }
+destination = "/etc/salt/grains"
+}
 
-  xml {
-    xslt = "${var.xslt}"
-  }
+provisioner "remote-exec" {
+  inline = [
+    "sh /root/salt/first_deployment_highstate.sh",
+  ]
+}
+
+xml {
+  xslt = var.xslt
+}
 }
 
 output "configuration" {
   value = {
-    id = "${libvirt_domain.domain.0.id}"
+    id       = libvirt_domain.domain[0].id
     hostname = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-1" : ""}.${var.base_configuration["domain"]}"
   }
 }
