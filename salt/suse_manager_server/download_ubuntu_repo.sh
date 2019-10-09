b9cc73d898f8465ca11992206334053f437b0f34
Michael Calmer <mc@suse.de>
2019-10-09 09:57:19 +0200

    fix setting up repo from minima mirror

--- salt/suse_manager_server/download_ubuntu_repo.sh
+++ salt/suse_manager_server/download_ubuntu_repo.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -e
+
 DIR=/srv/www/htdocs/pub/$1
 mkdir -p $DIR
 cd $DIR
-wget -r -np -A deb,dsc,tar.xz,tar.gz,gz,key,Packages,Release,Sources https://$2
+wget -r -np -A deb,dsc,tar.xz,tar.gz,gz,key,Packages,Release,Sources http://$2
 mv $2/* .
-rm -rf download.opensuse.org
+HOST=$(echo $2 | awk -F/ '{print $1}')
+if [ -n "$HOST" -a x"$HOST" != "x/" ]; then
+  rm -rf "$HOST"
+fi
 
