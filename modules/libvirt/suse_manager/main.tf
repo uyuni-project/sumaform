b6b58a650bc084486e92a74c5c12c855b53895ec
Silvio Moioli <moio@suse.com>
2019-10-15 11:26:15 +0200

    0.12: suse_manager module translation

--- modules/libvirt/suse_manager/main.tf
+++ modules/libvirt/suse_manager/main.tf
@@ -1,41 +1,45 @@
 variable "images" {
   default = {
-    "3.2-released" = "sles12sp4"
-    "3.2-nightly" = "sles12sp4"
-    "4.0-released" = "sles15sp1"
-    "4.0-nightly" = "sles15sp1"
-    "head" = "sles15sp2"
-    "uyuni-master" = "opensuse151"
+    "3.2-released"   = "sles12sp4"
+    "3.2-nightly"    = "sles12sp4"
+    "4.0-released"   = "sles15sp1"
+    "4.0-nightly"    = "sles15sp1"
+    "head"           = "sles15sp2"
+    "uyuni-master"   = "opensuse151"
     "uyuni-released" = "opensuse151"
   }
 }
 
 resource "libvirt_volume" "server_data_disk" {
-  name = "${var.base_configuration["name_prefix"]}${var.name}-server-data-disk"
-  size = "${var.repository_disk_size}"
-  pool = "${var.data_pool}"
+  name  = "${var.base_configuration["name_prefix"]}${var.name}-server-data-disk"
+  size  = "${var.repository_disk_size}"
+  pool  = "${var.data_pool}"
   count = "${var.repository_disk_size > 0 ? 1 : 0}"
 }
 
 module "suse_manager" {
   source = "../host"
 
-  base_configuration = "${var.base_configuration}"
-  name = "${var.name}"
-  use_os_released_updates = "${var.use_os_released_updates}"
-  use_os_unreleased_updates = "${var.use_os_unreleased_updates}"
-  additional_repos = "${var.additional_repos}"
-  additional_repos_only = "${var.additional_repos_only}"
-  additional_certs = "${var.additional_certs}"
-  additional_packages = "${var.additional_packages}"
-  swap_file_size = "${var.swap_file_size}"
-  ssh_key_path = "${var.ssh_key_path}"
-  gpg_keys = "${var.gpg_keys}"
-  ipv6 = "${var.ipv6}"
-  connect_to_base_network = true
+  base_configuration            = var.base_configuration
+  name                          = var.name
+  use_os_released_updates       = var.use_os_released_updates
+  use_os_unreleased_updates     = var.use_os_unreleased_updates
+  additional_repos              = var.additional_repos
+  additional_repos_only         = var.additional_repos_only
+  additional_certs              = var.additional_certs
+  additional_packages           = var.additional_packages
+  swap_file_size                = var.swap_file_size
+  ssh_key_path                  = var.ssh_key_path
+  gpg_keys                      = var.gpg_keys
+  ipv6                          = var.ipv6
+  connect_to_base_network       = true
   connect_to_additional_network = false
+
   # HACK: work around "conditional operator cannot be used with list values"
-  roles = "${split(",", var.register_to_server == "null" ? "suse_manager_server" : "suse_manager_server,minion")}"
+  roles = split(
+    ",",
+    var.register_to_server == "null" ? "suse_manager_server" : "suse_manager_server,minion",
+  )
   grains = <<EOF
 
 product_version: ${var.product_version}
@@ -82,29 +86,22 @@ repository_disk_device: vdb
 
 EOF
 
-  // Provider-specific variables
-  image = "${var.image == "default" ? lookup(var.images, var.product_version) : var.image}"
-  memory = "${var.memory}"
-  vcpu = "${var.vcpu}"
-  running = "${var.running}"
-  mac = "${var.mac}"
 
-  // HACK: Terraform 0.11 ternary operator is not short-circuiting
-  // https://github.com/hashicorp/terraform/issues/11566
-  // example by https://github.com/coreos/tectonic-installer/commit/0f209b31b169ce129ba457096b23792f6601d66e#diff-17708827fa84c637698cd840c01710c9R16
-  additional_disk = "${slice(
-    list(map("volume_id", join("",libvirt_volume.server_data_disk.*.id))),
-    0,
-    var.repository_disk_size > 0 ? 1 : 0
-  )}"
+  // Provider-specific variables
+  image           = var.image == "default" ? var.images[var.product_version] : var.image
+  memory          = var.memory
+  vcpu            = var.vcpu
+  running         = var.running
+  mac             = var.mac
+  additional_disk = var.repository_disk_size > 0 ? [{ volume_id = libvirt_volume.server_data_disk[0].id }] : []
 }
 
 output "configuration" {
   value = {
-    id = "${module.suse_manager.configuration["id"]}"
-    hostname = "${module.suse_manager.configuration["hostname"]}"
-    product_version = "${var.product_version}"
-    username = "${var.server_username}"
-    password = "${var.server_password}"
+    id              = module.suse_manager.configuration["id"]
+    hostname        = module.suse_manager.configuration["hostname"]
+    product_version = var.product_version
+    username        = var.server_username
+    password        = var.server_password
   }
 }
