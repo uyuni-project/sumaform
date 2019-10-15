3a3609b3ba3caef0e1955711f545c598bfa54c00
Silvio Moioli <moio@suse.com>
2019-10-15 11:20:22 +0200

    0.12: mirror module translation

--- modules/libvirt/mirror/main.tf
+++ modules/libvirt/mirror/main.tf
@@ -1,7 +1,7 @@
 resource "libvirt_volume" "data_disk" {
   name = "${var.base_configuration["name_prefix"]}mirror-data-disk"
   size = 1099511627776 # 1 TiB
-  pool = "${var.data_pool}"
+  pool = var.data_pool
   lifecycle {
     prevent_destroy = true
   }
@@ -10,14 +10,14 @@ resource "libvirt_volume" "data_disk" {
 module "mirror" {
   source = "../host"
 
-  base_configuration = "${var.base_configuration}"
-  name = "mirror"
-  additional_repos = "${var.additional_repos}"
-  additional_packages = "${var.additional_packages}"
-  swap_file_size = "${var.swap_file_size}"
-  ssh_key_path = "${var.ssh_key_path}"
-  roles = ["mirror"]
-  grains = <<EOF
+  base_configuration  = var.base_configuration
+  name                = "mirror"
+  additional_repos    = var.additional_repos
+  additional_packages = var.additional_packages
+  swap_file_size      = var.swap_file_size
+  ssh_key_path        = var.ssh_key_path
+  roles               = ["mirror"]
+  grains              = <<EOF
 
 cc_username: ${var.base_configuration["cc_username"]}
 cc_password: ${var.base_configuration["cc_password"]}
@@ -27,13 +27,17 @@ use_mirror_images: ${var.base_configuration["use_mirror_images"]}
 
 EOF
 
+
   // Provider-specific variables
-  image = "opensuse151"
-  memory = 512
-  vcpu = 1
-  running = "${var.running}"
-  mac = "${var.mac}"
-  additional_disk = [{
-    volume_id = "${libvirt_volume.data_disk.id}"
-  }]
+  image   = "opensuse151"
+  memory  = 512
+  vcpu    = 1
+  running = var.running
+  mac     = var.mac
+  additional_disk = [
+    {
+      volume_id = libvirt_volume.data_disk.id
+    },
+  ]
 }
+
