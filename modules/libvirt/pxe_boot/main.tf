0ace5c5127d845fb618fbc0438f812e67b382152
Silvio Moioli <moio@suse.com>
2019-10-15 11:20:48 +0200

    0.12: pxe_boot module automated translation

--- modules/libvirt/pxe_boot/main.tf
+++ modules/libvirt/pxe_boot/main.tf
@@ -1,7 +1,3 @@
-terraform {
-    required_version = "~> 0.12.9"
-}
-
 // Names are calculated as follows:
 // ${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index  + 1}" : ""}
 // This means:
@@ -9,45 +5,57 @@ terraform {
 //   name_prefix + name + "-" + index (if quantity > 1)
 
 resource "libvirt_volume" "main_disk" {
-  name = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index  + 1}" : ""}-main-disk"
+  name             = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index + 1}" : ""}-main-disk"
   base_volume_name = "${var.base_configuration["use_shared_resources"] ? "" : var.base_configuration["name_prefix"]}${var.image}"
-  pool = "${var.base_configuration["pool"]}"
-  count = "${var.quantity}"
+  pool             = var.base_configuration["pool"]
+  count            = var.quantity
 }
 
 resource "libvirt_domain" "domain" {
-  name = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index  + 1}" : ""}"
-  memory = "${var.memory}"
-  vcpu = "${var.vcpu}"
-  running = "${var.running}"
-  count = "${var.quantity}"
+  name       = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-${count.index + 1}" : ""}"
+  memory     = var.memory
+  vcpu       = var.vcpu
+  running    = var.running
+  count      = var.quantity
   qemu_agent = true
 
   // base disk + additional disks if any
-  disk = ["${concat(
-    list(
-      map("volume_id", "${element(libvirt_volume.main_disk.*.id, count.index)}")
-    ),
-    var.additional_disk
-  )}"]
-
-  network_interface = ["${
-    list(
-      map("wait_for_lease", false,
-          "network_id", var.base_configuration["additional_network_id"]
-         )
+  dynamic "disk" {
+    for_each = concat(
+      [
+        {
+          "volume_id" = element(libvirt_volume.main_disk.*.id, count.index)
+        },
+      ],
+      var.additional_disk,
     )
-  }"]
+    content {
+      volume_id = disk.value["volume_id"]
+    }
+  }
+
+  dynamic "network_interface" {
+    for_each = [
+      {
+        "wait_for_lease" = false
+        "network_id"     = var.base_configuration["additional_network_id"]
+      },
+    ]
+    content {
+      wait_for_lease = network_interface.value["wait_for_lease"]
+      network_id     = network_interface.value["network_id"]
+    }
+  }
 
   xml {
-    xslt = "${file("${path.module}/pxe.xsl")}"
+    xslt = file("${path.module}/pxe.xsl")
   }
 }
 
 output "configuration" {
   value = {
-    id = "${libvirt_domain.domain.0.id}"
+    id       = libvirt_domain.domain[0].id
     hostname = "${var.base_configuration["name_prefix"]}${var.name}${var.quantity > 1 ? "-1" : ""}.${var.base_configuration["domain"]}"
-    macaddr = "${libvirt_domain.domain.0.network_interface.0.mac}"
+    macaddr  = libvirt_domain.domain[0].network_interface[0].mac
   }
 }
--- modules/libvirt/pxe_boot/variables.tf
+++ modules/libvirt/pxe_boot/variables.tf
@@ -1,51 +1,52 @@
 variable "base_configuration" {
   description = "use module.base.configuration, see the main.tf example file"
-  type = "map"
+  type        = map(string)
 }
 
 variable "name" {
   description = "hostname, without the domain part"
-  type = "string"
+  type        = string
 }
 
-variable "quantity"  {
+variable "quantity" {
   description = "number of hosts like this one"
-  default = 1
+  default     = 1
 }
 
 // Provider-specific variables
 
 variable "image" {
   description = "One of: sles11sp4, sles12, sles12sp1, sles12sp2, sles12sp3, sles12sp4, sles15, centos7"
-  type = "string"
+  type        = string
 }
 
 variable "memory" {
   description = "RAM memory in MiB"
-  default = 1024
+  default     = 1024
 }
 
 variable "vcpu" {
   description = "Number of virtual CPUs"
-  default = 1
+  default     = 1
 }
 
 variable "running" {
   description = "Whether this host should be turned on or off"
-  default = true
+  default     = true
 }
 
 variable "mac" {
   description = "a MAC address in the form AA:BB:CC:11:22:22"
-  default = ""
+  default     = ""
 }
 
 variable "additional_disk" {
   description = "disk block definition(s) to be added to this host"
-  default = []
+  default     = []
 }
 
 variable "cpu_model" {
   description = "Define what CPU model the guest is getting (host-model, host-passthrough or the default)."
-  default = ""
+  default     = ""
 }
+
