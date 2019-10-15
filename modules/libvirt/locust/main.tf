bbd213001fcac2eec85b77ca9d0687c7eda2352a
Silvio Moioli <moio@suse.com>
2019-10-15 10:50:28 +0200

    0.12: locust module translation

--- modules/libvirt/locust/main.tf
+++ modules/libvirt/locust/main.tf
@@ -1,10 +1,10 @@
 module "locust" {
-  source = "../host"
-  base_configuration = "${var.base_configuration}"
-  name = "${var.name}"
-  ssh_key_path = "${var.ssh_key_path}"
-  roles = ["locust"]
-  grains = <<EOF
+  source             = "../host"
+  base_configuration = var.base_configuration
+  name               = var.name
+  ssh_key_path       = var.ssh_key_path
+  roles              = ["locust"]
+  grains             = <<EOF
 
 mirror: ${var.base_configuration["mirror"]}
 server: ${var.server_configuration["hostname"]}
@@ -15,21 +15,22 @@ locust_master_host: null
 locust_slave_count: ${var.slave_quantity}
 EOF
 
+
   // Provider-specific variables
-  image = "opensuse151"
-  memory = "${var.memory}"
-  running = "${var.running}"
-  mac = "${var.mac}"
+  image   = "opensuse151"
+  memory  = var.memory
+  running = var.running
+  mac     = var.mac
 }
 
 module "locust-slave" {
-  source = "../host"
-  base_configuration = "${var.base_configuration}"
-  name = "${var.name}-slave"
-  quantity = "${var.slave_quantity}"
-  ssh_key_path = "${var.ssh_key_path}"
-  roles = ["locust"]
-  grains = <<EOF
+  source             = "../host"
+  base_configuration = var.base_configuration
+  name               = "${var.name}-slave"
+  quantity           = var.slave_quantity
+  ssh_key_path       = var.ssh_key_path
+  roles              = ["locust"]
+  grains             = <<EOF
 
 mirror: ${var.base_configuration["mirror"]}
 server: ${var.server_configuration["hostname"]}
@@ -39,16 +40,17 @@ server_password: ${var.server_configuration["password"]}
 locust_master_host: ${module.locust.configuration["hostname"]}
 EOF
 
-  // Provider-specific variables
-  image = "opensuse151"
-  memory = "${var.memory}"
-  running = "${var.running}"
-}
 
+  // Provider-specific variables
+  image   = "opensuse151"
+  memory  = var.memory
+  running = var.running
+}
 
 output "configuration" {
   value = {
-    id = "${module.locust.configuration["id"]}"
-    hostname = "${module.locust.configuration["hostname"]}"
+    id       = module.locust.configuration["id"]
+    hostname = module.locust.configuration["hostname"]
   }
 }
+
