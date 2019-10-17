ae23a50d4272c879a7b340da391f3e9d165bff2e
Joachim Gleissner <jgleissner@suse.com>
2019-10-17 14:21:48 +0100

    Added pubcloud specific minima mirror config.
    This should be suitable for use with the Manager 4.0 test suite.

--- salt/mirror/mirror.sh
+++ salt/mirror/mirror.sh
@@ -8,6 +8,24 @@ minima -c /root/minima.yaml sync 2>&1 | tee /var/log/minima.log
 
 apt-mirror
 
+# check for nvidia repo and create links, if necessary
+if [ -d suse ]; then
+  mkdir -p RPMMD
+  for dir in suse/sle* ; do
+    d=${dir^^}
+    ver=${d#SUSE/SLE}
+    if [[ $ver == ${ver%SP*} ]]; then
+      target="${ver}-GA"
+    else
+      target=${ver/SP/-SP}
+    fi
+    full_path=RPMMD/${target}-Desktop-NVIDIA-Driver
+    if [ ! -e ${full_path} ]; then
+      ln -s ../suse/$dir ${full_path}
+    fi
+  done
+fi
+
 jdupes --linkhard -r -s /srv/mirror/
 
-chmod -R 777 .
+#chmod -R 777 .
