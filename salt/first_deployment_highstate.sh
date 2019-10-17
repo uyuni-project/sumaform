ba3949b557081efc4a438b4ac2f93659e588844d
Joachim Gleissner <jgleissner@suse.com>
2019-10-17 14:33:15 +0100

    Save salt deployment log for easier debugging

--- salt/first_deployment_highstate.sh
+++ salt/first_deployment_highstate.sh
@@ -3,6 +3,7 @@
 # Applies the "minimal" state in isolation to set the hostname and update Salt itself
 # then applies the highstate
 
+exec &> >(tee /var/log/highstate.log)
 salt-call --local --file-root=/root/salt/ --log-level=quiet --output=quiet state.sls default.minimal ||:
 salt-call --local --file-root=/root/salt/ --log-level=info --retcode-passthrough --force-color state.highstate || exit 1
 
