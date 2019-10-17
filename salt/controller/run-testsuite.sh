7f3fb8123e2d460ebadb54e54b715d411c5a067f
Joachim Gleissner <jgleissner@suse.com>
2019-10-17 14:34:16 +0100

    run-testsuite: sync channels if configured
    Before running init_clients set, sync configured channels from mirror to
    manager server. This may be required for the next test set to pass.

--- salt/controller/run-testsuite.sh
+++ salt/controller/run-testsuite.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+sync_repos()
+{
+  test -z "$SERVER" && { echo "SERVER not set" ; exit 1 ; }
+  repos=`salt-call --out txt --local --file-root=/root/salt grains.get repos_to_sync  | cut -d' ' -f2- 2>/dev/null`
+
+  if [ -n "$repos" ]; then
+    cmd="ssh -o StrictHostKeyChecking=no $SERVER \"spacewalk-repo-sync --latest --type yum"
+    for i in $repos ; do
+      cmd="$cmd --channel $i"
+    done
+    cmd="$cmd\""
+    echo "syncing repos '$repos' on server"
+    eval $cmd
+  fi
+}
+
 set -e
 
 cd /root/spacewalk/testsuite
@@ -7,6 +23,7 @@ cd /root/spacewalk/testsuite
 if [ -z "$1" ]; then
    rake cucumber:core
    rake cucumber:reposync
+   sync_repos
    rake cucumber:init_clients
    rake cucumber:secondary
    rake cucumber:secondary_parallelizable
@@ -32,6 +49,7 @@ fi
 if [[ $1 = "parallel" ]]; then
    rake cucumber:core
    rake cucumber:reposync
+   sync_repos
    rake parallel:init_clients
    rake cucumber:secondary
    rake parallel:secondary_parallelizable
