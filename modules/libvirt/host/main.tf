cbbd93a565cdb444f120ab7abeb7aa662a8d9cd8
Silvio Moioli <moio@suse.com>
2019-11-15 17:11:59 +0100

    Refactoring: make ipv6 flags forward-compatible with 0.12

--- modules/libvirt/host/main.tf
+++ modules/libvirt/host/main.tf
@@ -140,7 +140,7 @@ connect_to_additional_network: ${var.connect_to_additional_network}
 reset_ids: true
 ipv6: {${join(
     ", ",
-    formatlist("'%s': '%s'", keys(var.ipv6), values(var.ipv6)),
+    formatlist("'%s': %s", keys(var.ipv6), values(var.ipv6)),
 )}}
 ${var.grains}
 
