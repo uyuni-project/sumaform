424292bc2d28a033e7561e673476e7db56f99426
Ricardo Mateus <rmateus@suse.com>
2019-11-19 11:03:55 +0000

    add extra test to load images variable

--- modules/cucumber_testsuite/main.tf
+++ modules/cucumber_testsuite/main.tf
@@ -30,7 +30,7 @@ locals {
   additional_repos = { for host_key in local.hosts :
   host_key => lookup(var.host_settings[host_key], "additional_repos", {}) if var.host_settings[host_key] != null }
   images = { for host_key in local.hosts :
-  host_key => lookup(var.host_settings[host_key], "image", null) if var.host_settings[host_key] != null }
+  host_key => lookup(var.host_settings[host_key], "image", "default") if var.host_settings[host_key] != null }
 }
 
 module "srv" {
