7e1ef92936f341971463f9a83cae34b23b633315
Silvio Moioli <moio@suse.com>
2019-12-12 15:09:39 +0100

    controller: do not fail when resource tuple is empty
    During or after destroy, outputs of resources with a count attribute
    may contain empty tuples, and that will fail at destroy or apply time.
    
    This patch makes sure to avoid indexing empty tuples. Note that previous
    code checked the non-emptiness of tuples indirectly, depending on input
    variables, but this fails to catch all cases - a 0 cound might either
    depend on input variables or from the destruction of earlier resources.
    With this patch we catch all cases.

--- modules/libvirt/controller/main.tf
+++ modules/libvirt/controller/main.tf
@@ -32,37 +32,37 @@ module "controller" {
     proxy         = var.proxy_configuration["hostname"]
     client        = length(var.client_configuration["hostnames"]) > 0 ? var.client_configuration["hostnames"][0] : null
     minion        = length(var.minion_configuration["hostnames"]) > 0 ? var.minion_configuration["hostnames"][0] : null
-    centos_minion = var.centos_configuration["hostnames"] != null ? var.centos_configuration["hostnames"][0] : null
-    ubuntu_minion = var.ubuntu_configuration["hostnames"] != null ? var.ubuntu_configuration["hostnames"][0] : null
-    ssh_minion        = var.sshminion_configuration["hostnames"] != null ? var.sshminion_configuration["hostnames"][0] : null
-    kvm_host          = var.kvmhost_configuration["hostnames"] != null ? var.kvmhost_configuration["hostnames"][0] : null
+    centos_minion = length(var.centos_configuration["hostnames"]) > 0 ? var.centos_configuration["hostnames"][0] : null
+    ubuntu_minion = length(var.ubuntu_configuration["hostnames"]) > 0 ? var.ubuntu_configuration["hostnames"][0] : null
+    ssh_minion        = length(var.sshminion_configuration["hostnames"]) > 0 ? var.sshminion_configuration["hostnames"][0] : null
+    kvm_host          = length(var.kvmhost_configuration["hostnames"]) > 0 ? var.kvmhost_configuration["hostnames"][0] : null
     pxeboot_mac       = var.pxeboot_configuration["macaddr"]
     branch            = var.branch == "default" ? var.testsuite-branch[var.server_configuration["product_version"]] : var.branch
     git_profiles_repo = var.git_profiles_repo == "default" ? "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles" : var.git_profiles_repo
     server_http_proxy = var.server_http_proxy
 
-    sle11sp4_minion      = var.sle11sp4_minion_configuration["hostnames"] != null ? var.sle11sp4_minion_configuration["hostnames"][0] : null
-    sle11sp4_sshminion   = var.sle11sp4_sshminion_configuration["hostnames"] != null ? var.sle11sp4_sshminion_configuration["hostnames"][0] : null
-    sle11sp4_client      = var.sle11sp4_client_configuration["hostnames"] != null ? var.sle11sp4_client_configuration["hostnames"][0] : null
-    sle12sp4_minion      = var.sle12sp4_minion_configuration["hostnames"] != null ? var.sle12sp4_minion_configuration["hostnames"][0] : null
-    sle12sp4_sshminion   = var.sle12sp4_sshminion_configuration["hostnames"] != null ? var.sle12sp4_sshminion_configuration["hostnames"][0] : null
-    sle12sp4_client      = var.sle12sp4_client_configuration["hostnames"] != null ? var.sle12sp4_client_configuration["hostnames"][0] : null
-    sle15_minion         = var.sle15_minion_configuration["hostnames"] != null ? var.sle15_minion_configuration["hostnames"][0] : null
-    sle15_sshminion      = var.sle15_sshminion_configuration["hostnames"] != null ? var.sle15_sshminion_configuration["hostnames"][0] : null
-    sle15_client         = var.sle15_client_configuration["hostnames"] != null ? var.sle15_client_configuration["hostnames"][0] : null
-    sle15sp1_minion      = var.sle15sp1_minion_configuration["hostnames"] != null ? var.sle15sp1_minion_configuration["hostnames"][0] : null
-    sle15sp1_sshminion   = var.sle15sp1_sshminion_configuration["hostnames"] != null ? var.sle15sp1_sshminion_configuration["hostnames"][0] : null
-    sle15sp1_client      = var.sle15sp1_client_configuration["hostnames"] != null ? var.sle15sp1_client_configuration["hostnames"][0] : null
-    centos6_minion       = var.centos6_minion_configuration["hostnames"] != null ? var.centos6_minion_configuration["hostnames"][0] : null
-    centos6_sshminion    = var.centos6_sshminion_configuration["hostnames"] != null ? var.centos6_sshminion_configuration["hostnames"][0] : null
-    centos6_client       = var.centos6_client_configuration["hostnames"] != null ? var.centos6_client_configuration["hostnames"][0] : null
-    centos7_minion       = var.centos7_minion_configuration["hostnames"] != null ? var.centos7_minion_configuration["hostnames"][0] : null
-    centos7_sshminion    = var.centos7_sshminion_configuration["hostnames"] != null ? var.centos7_sshminion_configuration["hostnames"][0] : null
-    centos7_client       = var.centos7_client_configuration["hostnames"] != null ? var.centos7_client_configuration["hostnames"][0] : null
-    ubuntu1604_minion    = var.ubuntu1604_minion_configuration["hostnames"] != null ? var.ubuntu1604_minion_configuration["hostnames"][0] : null
-    ubuntu1604_sshminion = var.ubuntu1604_sshminion_configuration["hostnames"] != null ? var.ubuntu1604_sshminion_configuration["hostnames"][0] : null
-    ubuntu1804_minion    = var.ubuntu1804_minion_configuration["hostnames"] != null ? var.ubuntu1804_minion_configuration["hostnames"][0] : null
-    ubuntu1804_sshminion = var.ubuntu1804_sshminion_configuration["hostnames"] != null ? var.ubuntu1804_sshminion_configuration["hostnames"][0] : null
+    sle11sp4_minion      = length(var.sle11sp4_minion_configuration["hostnames"]) > 0 ? var.sle11sp4_minion_configuration["hostnames"][0] : null
+    sle11sp4_sshminion   = length(var.sle11sp4_sshminion_configuration["hostnames"]) > 0 ? var.sle11sp4_sshminion_configuration["hostnames"][0] : null
+    sle11sp4_client      = length(var.sle11sp4_client_configuration["hostnames"]) > 0 ? var.sle11sp4_client_configuration["hostnames"][0] : null
+    sle12sp4_minion      = length(var.sle12sp4_minion_configuration["hostnames"]) > 0 ? var.sle12sp4_minion_configuration["hostnames"][0] : null
+    sle12sp4_sshminion   = length(var.sle12sp4_sshminion_configuration["hostnames"]) > 0 ? var.sle12sp4_sshminion_configuration["hostnames"][0] : null
+    sle12sp4_client      = length(var.sle12sp4_client_configuration["hostnames"]) > 0 ? var.sle12sp4_client_configuration["hostnames"][0] : null
+    sle15_minion         = length(var.sle15_minion_configuration["hostnames"]) > 0 ? var.sle15_minion_configuration["hostnames"][0] : null
+    sle15_sshminion      = length(var.sle15_sshminion_configuration["hostnames"]) > 0 ? var.sle15_sshminion_configuration["hostnames"][0] : null
+    sle15_client         = length(var.sle15_client_configuration["hostnames"]) > 0 ? var.sle15_client_configuration["hostnames"][0] : null
+    sle15sp1_minion      = length(var.sle15sp1_minion_configuration["hostnames"]) > 0 ? var.sle15sp1_minion_configuration["hostnames"][0] : null
+    sle15sp1_sshminion   = length(var.sle15sp1_sshminion_configuration["hostnames"]) > 0 ? var.sle15sp1_sshminion_configuration["hostnames"][0] : null
+    sle15sp1_client      = length(var.sle15sp1_client_configuration["hostnames"]) > 0 ? var.sle15sp1_client_configuration["hostnames"][0] : null
+    centos6_minion       = length(var.centos6_minion_configuration["hostnames"]) > 0 ? var.centos6_minion_configuration["hostnames"][0] : null
+    centos6_sshminion    = length(var.centos6_sshminion_configuration["hostnames"]) > 0 ? var.centos6_sshminion_configuration["hostnames"][0] : null
+    centos6_client       = length(var.centos6_client_configuration["hostnames"]) > 0 ? var.centos6_client_configuration["hostnames"][0] : null
+    centos7_minion       = length(var.centos7_minion_configuration["hostnames"]) > 0 ? var.centos7_minion_configuration["hostnames"][0] : null
+    centos7_sshminion    = length(var.centos7_sshminion_configuration["hostnames"]) > 0 ? var.centos7_sshminion_configuration["hostnames"][0] : null
+    centos7_client       = length(var.centos7_client_configuration["hostnames"]) > 0 ? var.centos7_client_configuration["hostnames"][0] : null
+    ubuntu1604_minion    = length(var.ubuntu1604_minion_configuration["hostnames"]) > 0 ? var.ubuntu1604_minion_configuration["hostnames"][0] : null
+    ubuntu1604_sshminion = length(var.ubuntu1604_sshminion_configuration["hostnames"]) > 0 ? var.ubuntu1604_sshminion_configuration["hostnames"][0] : null
+    ubuntu1804_minion    = length(var.ubuntu1804_minion_configuration["hostnames"]) > 0 ? var.ubuntu1804_minion_configuration["hostnames"][0] : null
+    ubuntu1804_sshminion = length(var.ubuntu1804_sshminion_configuration["hostnames"]) > 0 ? var.ubuntu1804_sshminion_configuration["hostnames"][0] : null
   }
 
 
