1360a18385d0800a1374535cc07c4a2ec3de4ca6
Ricardo Mateus <rmateus@suse.com>
2020-01-21 14:45:20 +0000

    ssh backend support with provision
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>
    
    Update README.md
    
    Co-Authored-By: Silvio Moioli <moio@suse.com>
    
    Update backend_modules/ssh/README.md
    
    Co-Authored-By: Silvio Moioli <moio@suse.com>
    
    Update backend_modules/ssh/README.md
    
    Co-Authored-By: Silvio Moioli <moio@suse.com>
    
    Update backend_modules/ssh/README.md
    
    Co-Authored-By: Silvio Moioli <moio@suse.com>
    
    Update backend_modules/ssh/README.md
    
    Co-Authored-By: Silvio Moioli <moio@suse.com>
    
    Update backend_modules/ssh/README.md
    
    Co-Authored-By: Silvio Moioli <moio@suse.com>
    
    Update backend_modules/ssh/host/main.tf
    
    Co-Authored-By: Silvio Moioli <moio@suse.com>
    
    Update backend_modules/ssh/host/main.tf
    
    Co-Authored-By: Silvio Moioli <moio@suse.com>
    
    Update backend_modules/ssh/volume/main.tf
    
    Co-Authored-By: Silvio Moioli <moio@suse.com>
    
    fixup! ssh backend support with provision
    
    fixup! ssh backend support with provision
    
    fixup! ssh backend support with provision
    
    fixup! ssh backend support with provision
    
    fixup! ssh backend support with provision
    
    Update backend_modules/ssh/README.md
    
    Co-Authored-By: Silvio Moioli <smoioli@suse.de>

---
+++ backend_modules/ssh/base/main.tf
@@ -0,0 +1,49 @@
+
+resource "null_resource" "base" {
+  triggers = {
+    cc_username          = var.cc_username
+    cc_password          = var.cc_password
+    timezone             = var.timezone
+    ssh_key_path         = var.ssh_key_path
+    mirror               = var.mirror
+    use_mirror_images    = var.use_mirror_images
+    use_avahi            = var.use_avahi
+    domain               = var.domain
+    name_prefix          = var.name_prefix
+    use_shared_resources = var.use_shared_resources
+    testsuite            = var.testsuite
+    provider_settings    = yamlencode(var.provider_settings)
+  }
+}
+
+output "configuration" {
+  value = {
+    cc_username          = var.cc_username
+    cc_password          = var.cc_password
+    timezone             = var.timezone
+    ssh_key_path         = var.ssh_key_path
+    mirror               = var.mirror
+    use_mirror_images    = var.use_mirror_images
+    use_avahi            = var.use_avahi
+    domain               = var.domain
+    name_prefix          = var.name_prefix
+    use_shared_resources = var.use_shared_resources
+    testsuite            = var.testsuite
+
+    additional_network = lookup(var.provider_settings, "additional_network", null)
+
+    // only supported in ssh connection
+    private_key = lookup(var.provider_settings, "private_key", null)
+    certificate = lookup(var.provider_settings, "certificate", null)
+    host_key    = lookup(var.provider_settings, "host_key", null)
+
+    bastion_host        = lookup(var.provider_settings, "bastion_host", null)
+    bastion_host_key    = lookup(var.provider_settings, "bastion_host_key", null)
+    bastion_port        = lookup(var.provider_settings, "bastion_port", null)
+    bastion_user        = lookup(var.provider_settings, "bastion_user", null)
+    bastion_password    = lookup(var.provider_settings, "bastion_password", null)
+    bastion_private_key = lookup(var.provider_settings, "bastion_private_key", null)
+    bastion_certificate = lookup(var.provider_settings, "bastion_certificate", null)
+    timeout             = lookup(var.provider_settings, "timeout", "20s")
+  }
+}
---
+++ backend_modules/ssh/volume/main.tf
@@ -0,0 +1,6 @@
+// in ssh backend we don't provide any support for disk creation
+output "configuration" {
+  value = {
+    ids =  []
+  }
+}
