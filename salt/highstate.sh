795affeffe30b9c11bae7b10cfe81df7524856e3
Ricardo Mateus <rmateus@suse.com>
2020-02-17 17:15:41 +0000

    ssh backend provision: user different from root
    Allow to run provision using a sudo user without the beed to be the "root"
    Usefull, for example, for AWS where user is "ec2-user"
    
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- salt/highstate.sh
+++ salt/highstate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Applies the highstate - assumes the minimal state was already applied
+FILE_ROOT="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-salt-call --local --file-root=/root/salt/ --log-level=info --retcode-passthrough --force-color state.highstate
+salt-call --local --file-root=$FILE_ROOT/ --log-level=info --retcode-passthrough --force-color state.highstate
