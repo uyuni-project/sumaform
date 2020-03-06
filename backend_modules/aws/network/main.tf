a7cdb62c53b50f9355bde786449fd2f6d08682ba
Ricardo Mateus <rmateus@suse.com>
2020-03-06 15:24:21 +0000

    private network with outbound internet access
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- backend_modules/aws/network/main.tf
+++ backend_modules/aws/network/main.tf
@@ -14,25 +14,10 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public" {
-  availability_zone       = var.availability_zone
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.16.0.0/17"
-  map_public_ip_on_launch = true
-
+resource "aws_eip" "nat_eip" {
+  vpc = true
   tags = {
-    Name = "${var.name_prefix}-public-subnet"
-  }
-}
-
-resource "aws_subnet" "private" {
-  availability_zone       = var.availability_zone
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.16.128.0/17"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "${var.name_prefix}-private-subnet"
+    Name = "${var.name_prefix}-nat-eip"
   }
 }
 
@@ -44,7 +29,18 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_route_table" "internet" {
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  depends_on = [aws_internet_gateway.main]
+
+  tags = {
+    Name = "${var.name_prefix}-nat"
+  }
+}
+
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -53,13 +49,58 @@ resource "aws_route_table" "internet" {
   }
 
   tags = {
-    Name = "${var.name_prefix}-internet-route-table"
+    Name = "${var.name_prefix}-public-route-table"
   }
 }
 
 resource "aws_main_route_table_association" "vpc_internet" {
   vpc_id         = aws_vpc.main.id
-  route_table_id = aws_route_table.internet.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-private-route-table"
+  }
+}
+
+resource "aws_subnet" "public" {
+  availability_zone       = var.availability_zone
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.16.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name_prefix}-public-subnet"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "private" {
+  availability_zone       = var.availability_zone
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.16.1.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.name_prefix}-private-subnet"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_vpc_dhcp_options" "dhcp_options" {
@@ -127,7 +168,7 @@ resource "aws_security_group" "private" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [aws_vpc.main.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   lifecycle {
@@ -140,6 +181,7 @@ resource "aws_security_group" "private" {
 }
 
 output "configuration" {
+  depends_on = [aws_route_table_association.private, aws_route_table_association.public]
   value = {
     public_subnet_id          = aws_subnet.public.id
     private_subnet_id         = aws_subnet.private.id
