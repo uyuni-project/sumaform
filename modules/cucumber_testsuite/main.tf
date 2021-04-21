54b466b090135c18a903f8717fcc21d75d183d58
Michael Calmer <mc@suse.de>
2021-04-21 14:51:23 +0200

    support additional_packages in cucumber_testsuite module

--- modules/cucumber_testsuite/main.tf
+++ modules/cucumber_testsuite/main.tf
@@ -23,6 +23,8 @@ locals {
     host_key => lookup(var.host_settings[host_key], "provider_settings", {}) if var.host_settings[host_key] != null }
   additional_repos          = { for host_key in local.hosts :
     host_key => lookup(var.host_settings[host_key], "additional_repos", {}) if var.host_settings[host_key] != null }
+  additional_packages       = { for host_key in local.hosts :
+    host_key => lookup(var.host_settings[host_key], "additional_packages", []) if var.host_settings[host_key] != null }
   images                    = { for host_key in local.hosts :
     host_key => lookup(var.host_settings[host_key], "image", "default") if var.host_settings[host_key] != null ? contains(keys(var.host_settings[host_key]), "image") : false }
   names                     = { for host_key in local.hosts :
@@ -52,6 +54,7 @@ module "server" {
   ssh_key_path                   = "./salt/controller/id_rsa.pub"
   from_email                     = var.from_email
   additional_repos               = lookup(local.additional_repos, "server", {})
+  additional_packages            = lookup(local.additional_packages, "server", [])
 
   saltapi_tcpdump   = var.saltapi_tcpdump
   provider_settings = lookup(local.provider_settings_by_host, "server", {})
@@ -79,6 +82,7 @@ module "proxy" {
   ssh_key_path              = "./salt/controller/id_rsa.pub"
 
   additional_repos  = lookup(local.additional_repos, "proxy", {})
+  additional_packages = lookup(local.additional_packages, "proxy", [])
   provider_settings = lookup(local.provider_settings_by_host, "proxy", {})
 }
 
@@ -103,6 +107,7 @@ module "suse-client" {
   ssh_key_path            = "./salt/controller/id_rsa.pub"
 
   additional_repos  = lookup(local.additional_repos, "suse-client", {})
+  additional_packages = lookup(local.additional_packages, "suse-client", [])
   provider_settings = lookup(local.provider_settings_by_host, "suse-client", {})
 }
 
@@ -122,6 +127,7 @@ module "suse-minion" {
   ssh_key_path            = "./salt/controller/id_rsa.pub"
 
   additional_repos  = lookup(local.additional_repos, "suse-minion", {})
+  additional_packages = lookup(local.additional_packages, "suse-minion", [])
   provider_settings = lookup(local.provider_settings_by_host, "suse-minion", {})
 }
 
@@ -142,6 +148,7 @@ module "build-host" {
   avahi_reflector         = var.avahi_reflector
 
   additional_repos  = lookup(local.additional_repos, "build-host", {})
+  additional_packages = lookup(local.additional_packages, "build-host", [])
   provider_settings = lookup(local.provider_settings_by_host, "build-host", {})
 }
 
@@ -160,6 +167,7 @@ module "suse-sshminion" {
   gpg_keys                = ["default/gpg_keys/galaxy.key"]
 
   additional_repos  = lookup(local.additional_repos, "suse-sshminion", {})
+  additional_packages = lookup(local.additional_packages, "suse-sshminion", [])
   provider_settings = lookup(local.provider_settings_by_host, "suse-sshminion", {})
 }
 
@@ -178,6 +186,7 @@ module "redhat-minion" {
   ssh_key_path           = "./salt/controller/id_rsa.pub"
 
   additional_repos  = lookup(local.additional_repos, "redhat-minion", {})
+  additional_packages = lookup(local.additional_packages, "redhat-minion", [])
   provider_settings = lookup(local.provider_settings_by_host, "redhat-minion", {})
 }
 
@@ -196,6 +205,7 @@ module "debian-minion" {
   ssh_key_path           = "./salt/controller/id_rsa.pub"
 
   additional_repos  = lookup(local.additional_repos, "debian-minion", {})
+  additional_packages = lookup(local.additional_packages, "debian-minion", [])
   provider_settings = lookup(local.provider_settings_by_host, "debian-minion", {})
 }
 
@@ -227,6 +237,7 @@ module "kvm-host" {
   ssh_key_path            = "./salt/controller/id_rsa.pub"
 
   additional_repos  = lookup(local.additional_repos, "kvm-host", {})
+  additional_packages = lookup(local.additional_packages, "kvm-host", [])
   provider_settings = lookup(local.provider_settings_by_host, "kvm-host", {})
 }
 
@@ -248,6 +259,7 @@ module "xen-host" {
   ssh_key_path            = "./salt/controller/id_rsa.pub"
 
   additional_repos  = lookup(local.additional_repos, "xen-host", {})
+  additional_packages = lookup(local.additional_packages, "xen-host", [])
   provider_settings = lookup(local.provider_settings_by_host, "xen-host", {})
 }
 
@@ -281,6 +293,7 @@ module "controller" {
   swap_file_size         = null
 
   additional_repos  = lookup(local.additional_repos, "controller", {})
+  additional_packages = lookup(local.additional_packages, "controller", [])
   provider_settings = lookup(local.provider_settings_by_host, "controller", {})
 }
 
