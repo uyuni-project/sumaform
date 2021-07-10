7dfd5f1233d516afe764c92a64fc1d8c1f551727
Eric Bischoff <ebischoff@suse.com>
2021-07-10 09:59:49 +0200

    Log file name

--- salt/mirror/cron_scripts/docker-images.sh
+++ salt/mirror/cron_scripts/docker-images.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
   
-/usr/bin/date >> /var/log/docker_images.log
-echo '---------------------------------------------------------' >> /var/log/docker_images.log
-(/usr/local/bin/utils/docker_images /etc/docker-images.conf 2>&1) >> /var/log/docker_images.log
-echo >> /var/log/docker_images.log
+/usr/bin/date >> /var/log/docker-images.log
+echo '---------------------------------------------------------' >> /var/log/docker-images.log
+(/usr/local/bin/utils/docker_images /etc/docker-images.conf 2>&1) >> /var/log/docker-images.log
+echo >> /var/log/docker-images.log
