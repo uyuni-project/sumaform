bd1b2586b9697011f50bd61e17ad83c10b1142ce
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-09-16 10:01:50 +0200

    Use custom image for openSUSE clients when testing PRs
    openSUSE official images are being constantly being updated. This
    creates 2 problems:
    
    1- The image can be different from build to build
    2- Sometimes the image is not available
    
    Given this is for testing PRs, we don't need to use the official one.
    For master testing, yes, but for PRs, we are allowed to take some
    shortcuts that add stability or velocity.
    
    In this case, what we are doing is building the image once and disabling
    the build. This way, once the image has been synced in all mirrors, there won't
    be new builds.
    
    Thus, by using our image, every build will have the same image, which
    adds consistency and test stability.
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- backend_modules/libvirt/base/main.tf
+++ backend_modules/libvirt/base/main.tf
@@ -11,6 +11,7 @@ locals {
     opensuse152-ci-pr  = "${var.use_mirror_images ? "http://${var.mirror}" : "http://download.opensuse.org"}/repositories/systemsmanagement:/sumaform:/images:/libvirt/images/opensuse152-ci-pr.x86_64.qcow2"
     opensuse152o = "${var.use_mirror_images ? "http://${var.mirror}" : "http://download.opensuse.org"}/distribution/leap/15.2/appliances/openSUSE-Leap-15.2-JeOS.x86_64-OpenStack-Cloud.qcow2"
     opensuse153-ci-pr  = "${var.use_mirror_images ? "http://${var.mirror}" : "http://download.opensuse.org"}/repositories/systemsmanagement:/sumaform:/images:/libvirt/images/opensuse153-ci-pr.x86_64.qcow2"
+    opensuse153-ci-pr-client  = "${var.use_mirror_images ? "http://${var.mirror}" : "http://download.opensuse.org"}/repositories/systemsmanagement:/sumaform:/images:/libvirt/images/opensuse153-ci-pr-client.x86_64.qcow2"
     opensuse153o = "${var.use_mirror_images ? "http://${var.mirror}" : "http://download.opensuse.org"}/distribution/leap/15.3/appliances/openSUSE-Leap-15.3-JeOS.x86_64-OpenStack-Cloud.qcow2"
     sles15       = "${var.use_mirror_images ? "http://${var.mirror}" : "http://download.suse.de"}/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles15.x86_64.qcow2"
     sles15o      = "${var.use_mirror_images ? "http://${var.mirror}" : "http://download.suse.de"}/install/SLE-15-JeOS-GM/SLES15-JeOS.x86_64-15.0-OpenStack-Cloud-GM.qcow2"
