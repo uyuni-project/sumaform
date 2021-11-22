f2ccfaf21ad45554d425e7105f3e57e6767feef5
Oscar Barrios <obarrios@suse.com>
2021-11-22 12:23:07 +0100

    Just trying to overwrite versions.tf

---
+++ backend_modules/libvirt/client/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = "1.0.10"
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.6.3"
+    }
+  }
+}
--- modules/grafana/versions.tf
+++ modules/grafana/versions.tf
@@ -1,10 +1,3 @@
-
 terraform {
   required_version = "1.0.10"
-  required_providers {
-    libvirt = {
-      source = "dmacvicar/libvirt"
-      version = "0.6.3"
-    }
-  }
 }
