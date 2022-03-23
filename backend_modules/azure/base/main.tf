f79e68da4baa874943b61bddb5c56a38de698886
Maxime Noel <maxime.noel@suse.com>
2022-03-23 16:35:02 +1300

    Add image support to azure + fix user_data for different images Add SUSEConnect clean

--- backend_modules/azure/base/main.tf
+++ backend_modules/azure/base/main.tf
@@ -17,25 +17,116 @@ locals {
   key_file = lookup(var.provider_settings, "key_file", null)
 }
 
-data "azurerm_platform_image" "opensuse152" {
+data "azurerm_platform_image" "opensuse152o" {
   location  = local.location
   publisher = "suse"
   offer     = "opensuse-leap"
-  sku       = "15-2"
+  sku       = "15-2-gen2"
 }
 
-data "azurerm_platform_image" "opensuse153" {
+data "azurerm_platform_image" "opensuse153o" {
   location  = local.location
   publisher = "suse"
-  offer     = "opensuse-leap"
-  sku       = "15-3"
+  offer     = "opensuse-leap-15-3"
+  sku       = "gen1"
 }
 
-data "azurerm_platform_image" "sles15sp2" {
+data "azurerm_platform_image" "sles15" {
+  location  = local.location
+  publisher = "suse"
+  offer     = "SLES-BYOS"
+  sku       = "15"
+}
+
+data "azurerm_platform_image" "sles12sp3" {
+  location  = local.location
+  publisher = "suse"
+  offer     = "SLES-BYOS"
+  sku       = "12-SP3"
+}
+
+data "azurerm_platform_image" "sles12sp4" {
+  location  = local.location
+  publisher = "suse"
+  offer     = "SLES-BYOS"
+  sku       = "12-SP4"
+}
+
+data "azurerm_platform_image" "sles11sp4" {
+  location  = local.location
+  publisher = "suse"
+  offer     = "SLES-BYOS"
+  sku       = "11-SP4"
+}
+
+data "azurerm_platform_image" "sles15sp1o" {
+  location  = local.location
+  publisher = "suse"
+  offer     = "sles-15-sp1-byos"
+  sku       = "gen1"
+}
+
+data "azurerm_platform_image" "sles15sp2o" {
   location  = local.location
   publisher = "suse"
   offer     = "sles-15-sp2-byos"
   sku       = "gen1"
+}
+
+data "azurerm_platform_image" "sles15sp3o" {
+  location  = local.location
+  publisher = "suse"
+  offer     = "sles-15-sp3-byos"
+  sku       = "gen1"
+}
+
+data "azurerm_platform_image" "centos6" {
+  location  = local.location
+  publisher = "OpenLogic"
+  offer     = "CentOS"
+  sku       = "6.10"
+}
+
+data "azurerm_platform_image" "centos7" {
+  location  = local.location
+  publisher = "OpenLogic"
+  offer     = "CentOS-LVM"
+  sku       = "7-LVM"
+}
+
+data "azurerm_platform_image" "rhel7" {
+  location  = local.location
+  publisher = "RedHat"
+  offer     = "RHEL"
+  sku       = "7-LVM"
+}
+
+data "azurerm_platform_image" "rhel8" {
+  location  = local.location
+  publisher = "RedHat"
+  offer     = "RHEL"
+  sku       = "8-LVM"
+}
+
+data "azurerm_platform_image" "ubuntu1604" {
+  location  = local.location
+  publisher = "cognosys"
+  offer     = "ubuntu-1604-lts"
+  sku       = "ubuntu-16-04-lts"
+}
+
+data "azurerm_platform_image" "ubuntu1804" {
+  location  = local.location
+  publisher = "cognosys"
+  offer     = "ubuntu-1804-lts"
+  sku       = "ubuntu-18-04-lts"
+}
+
+data "azurerm_platform_image" "ubuntu2004" {
+  location  = local.location
+  publisher = "cognosys"
+  offer     = "ubuntu-20-04-lts"
+  sku       = "ubuntu-20-04-lts"
 }
 
 data "azurerm_platform_image" "suma41" {
@@ -45,6 +136,12 @@ data "azurerm_platform_image" "suma41" {
   sku       = "gen1"
 }
 
+data "azurerm_platform_image" "suma42" {
+  location  = local.location
+  publisher = "suse"
+  offer     = "manager-server-4-2-byos"
+  sku       = "gen1"
+}
 
 module "network" {
   source = "../network"
@@ -79,10 +176,25 @@ locals {
     key_file = local.key_file
     resource_group_name = module.network.configuration.resource_group_name
     platform_image_info = {
-      opensuse153 = { platform_image = data.azurerm_platform_image.opensuse153 },
-      opensuse152 = { platform_image = data.azurerm_platform_image.opensuse152 },
-      sles15sp2   = { platform_image = data.azurerm_platform_image.sles15sp2 },
-      suma41 = { platform_image = data.azurerm_platform_image.suma41 },
+      opensuse153o = { platform_image = data.azurerm_platform_image.opensuse153o },
+      opensuse152o = { platform_image = data.azurerm_platform_image.opensuse152o },
+      sles15       = { platform_image = data.azurerm_platform_image.sles15 },
+      sles15sp1o   = { platform_image = data.azurerm_platform_image.sles15sp1o },
+      sles15sp2o   = { platform_image = data.azurerm_platform_image.sles15sp2o },
+      sles15sp3o   = { platform_image = data.azurerm_platform_image.sles15sp3o },
+      sles12sp4    = { platform_image = data.azurerm_platform_image.sles12sp4 },
+      sles12sp3    = { platform_image = data.azurerm_platform_image.sles12sp3 },
+      sles11sp4    = { platform_image = data.azurerm_platform_image.sles11sp4 },
+      centos6      = { platform_image = data.azurerm_platform_image.centos6 },
+      centos7      = { platform_image = data.azurerm_platform_image.centos7 },
+      ubuntu2004   = { platform_image = data.azurerm_platform_image.ubuntu2004 },
+      ubuntu1804   = { platform_image = data.azurerm_platform_image.ubuntu1804 },
+      ubuntu1604   = { platform_image = data.azurerm_platform_image.ubuntu1604 },
+      rhel8        = { platform_image = data.azurerm_platform_image.rhel8 },
+      rhel7        = { platform_image = data.azurerm_platform_image.rhel7 },
+      suma41       = { platform_image = data.azurerm_platform_image.suma41 },
+      suma42       = { platform_image = data.azurerm_platform_image.suma42 }
+
     }
     },
     local.create_network ? module.network.configuration : {
@@ -100,7 +212,7 @@ locals {
   source                        = "../host"
   quantity                      = local.create_network ? 1 : 0
   base_configuration            = local.configuration_output
-  image                         = "opensuse152"
+  image                         = "opensuse152o"
   name                          = "bastion"
   connect_to_additional_network = true
   provider_settings = {
