397a5e026fa5674002d9e72cdf9df2cffa41a4cc
Maxime Noel <maxime.noel@suse.com>
2022-10-26 15:11:36 +1300

    change owner for ubuntu

--- backend_modules/aws/base/ami.tf
+++ backend_modules/aws/base/ami.tf
@@ -236,7 +236,7 @@ data "aws_ami" "centos7" {
 data "aws_ami" "ubuntu2204" {
   most_recent = true
   name_regex  = "^ubuntu/images/hvm-ssd/ubuntu-jammy-22.04"
-  owners      = ["099720109477"]
+  owners      = ["679593333241"]
 
   filter {
     name   = "architecture"
