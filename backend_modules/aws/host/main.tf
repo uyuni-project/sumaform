d184ca4480eae686c895b5655ff49d4f3eeb03e0
Maxime Noel <maxime.noel@suse.com>
2022-11-03 14:31:49 +1300

    Reenable eip

--- backend_modules/aws/host/main.tf
+++ backend_modules/aws/host/main.tf
@@ -7,7 +7,7 @@ locals {
     key_file        = var.base_configuration["key_file"]
     ssh_user        = lookup(lookup(var.base_configuration["ami_info"], var.image, {}), "ssh_user", "ec2-user")
     public_instance = false
-//    instance_with_eip = false
+    instance_with_eip = false
     volume_size     = 50
     bastion_host    = lookup(var.base_configuration, "bastion_host", null)
     instance_type = "t3.micro" },
@@ -35,7 +35,7 @@ locals {
   region            = var.base_configuration["region"]
   data_disk_device = split(".", local.provider_settings["instance_type"])[0] == "t2" ? "xvdf" : "nvme1n1"
 
-//  host_eip = local.provider_settings["public_instance"] && local.provider_settings["instance_with_eip"]? true: false
+  host_eip = local.provider_settings["public_instance"] && local.provider_settings["instance_with_eip"]? true: false
 }
 
 data "template_file" "user_data" {
@@ -48,22 +48,22 @@ data "template_file" "user_data" {
   }
 }
 
-//resource "aws_eip" "host_eip" {
-//  count = local.host_eip ? var.quantity : 0
-//
-//  vpc = true
-//  tags = {
-//    Name = "${local.resource_name_prefix}-host-eip${var.quantity > 1 ? "-${count.index + 1}" : ""}"
-//
-//  }
-//}
-//
-//resource "aws_eip_association" "eip_assoc" {
-//  count = local.host_eip ? var.quantity : 0
-//  allocation_id = aws_eip.host_eip[count.index].id
-//  #instance_id   = aws_instance.instance[count.index].id
-//  network_interface_id = aws_instance.instance[count.index].primary_network_interface_id
-//}
+resource "aws_eip" "host_eip" {
+  count = local.host_eip ? var.quantity : 0
+
+  vpc = true
+  tags = {
+    Name = "${local.resource_name_prefix}-host-eip${var.quantity > 1 ? "-${count.index + 1}" : ""}"
+
+  }
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  count = local.host_eip ? var.quantity : 0
+  allocation_id = aws_eip.host_eip[count.index].id
+  #instance_id   = aws_instance.instance[count.index].id
+  network_interface_id = aws_instance.instance[count.index].primary_network_interface_id
+}
 
 resource "aws_instance" "instance" {
   ami                    = local.ami
