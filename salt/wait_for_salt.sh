84c94791ded304ce624056afb426aa0c894308a6
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2023-03-09 09:16:11 +0000

    Increase retries when waiting cloud-init to finish

--- salt/wait_for_salt.sh
+++ salt/wait_for_salt.sh
@@ -3,16 +3,16 @@
 if [ -x /usr/bin/cloud-init ]; then
     # Wait for cloud-init to finish
     NEXT_TRY=0
-    until [ $NEXT_TRY -eq 10 ] || ! cloud-init status | grep running
+    until [ $NEXT_TRY -eq 50 ] || ! cloud-init status | grep running
     do
             echo "cloud-init is still running. Retrying... [$NEXT_TRY]";
             sleep 10;
             ((NEXT_TRY++));
     done
 
-    if [ $NEXT_TRY -eq 10 ]
+    if [ $NEXT_TRY -eq 50 ]
     then
-            echo "cloud-init is still running after 10 retries";
+            echo "ERROR: cloud-init is still running after 50 retries";
             exit 1;
     fi
 fi
