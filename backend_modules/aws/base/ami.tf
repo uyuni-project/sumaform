8f651a5adfe3e4fcb261a27bc58b004d010f7799
Maxime Noel <maxime.noel@suse.com>
2023-11-23 15:31:58 +1300

    Add sle12sp5 and sap15sp5 paygo image

--- backend_modules/aws/base/ami.tf
+++ backend_modules/aws/base/ami.tf
@@ -166,27 +166,6 @@ data "aws_ami" "sles15sp5-paygo" {
   }
 }
 
-data "aws_ami" "sap-sles15sp5-paygo" {
-  most_recent = true
-  name_regex  = "^suse-sles-sap-15-sp5-v"
-  owners      = ["679593333241"] // aws-marketplace
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
 data "aws_ami" "suma-server-43-byos" {
   most_recent = true
   name_regex  = "^suse-manager-server-4-3-byos-v"
@@ -254,6 +233,27 @@ data "aws_ami" "sles12sp5" {
   most_recent = true
   name_regex  = "^suse-sles-12-sp5-byos-v"
   owners      = ["679593333241"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_ami" "sles12sp5-paygo" {
+  most_recent = true
+  name_regex  = "^suse-sles-12-sp5-v"
+  owners      = ["013907871322"] // aws marketplace
 
   filter {
     name   = "architecture"
@@ -380,6 +380,27 @@ data "aws_ami" "rhel9" {
   most_recent = true
   name_regex  = "^RHEL-9.2.0_HVM-"
   owners      = ["309956199498"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_ami" "sap15sp5-paygo" {
+  most_recent = true
+  name_regex  = "^suse-sles-sap-15-sp5-v"
+  owners      = ["679593333241"]
 
   filter {
     name   = "architecture"
