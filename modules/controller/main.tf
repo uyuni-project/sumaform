59d7a31c2efaa46d09591ae1101931c143f4edcb
maximenoel8 <55169628+maximenoel8@users.noreply.github.com>
2023-11-27 09:10:02 +1300

    Add support to paygo images on controller (#1426)

--- modules/controller/main.tf
+++ modules/controller/main.tf
@@ -60,10 +60,15 @@ module "controller" {
     server_http_proxy         = var.server_http_proxy
     custom_download_endpoint  = var.custom_download_endpoint
     pxeboot_image             = var.pxeboot_configuration["image"]
+    is_using_paygo_server     = var.is_using_paygo_server
     is_using_build_image      = var.is_using_build_image
     is_using_scc_repositories = var.is_using_scc_repositories
+    server_instance_id        = var.server_instance_id
     container_runtime         = lookup(var.server_configuration, "runtime", null)
 
+    sle12_paygo_minion    = length(var.sle12_paygo_minion_configuration["hostnames"]) > 0 ? var.sle12_paygo_minion_configuration["hostnames"][0] : null
+    sle15_paygo_minion    = length(var.sle15_paygo_minion_configuration["hostnames"]) > 0 ? var.sle15_paygo_minion_configuration["hostnames"][0] : null
+    slesforsap15_paygo_minion = length(var.sleforsap15_paygo_minion_configuration["hostnames"]) > 0 ? var.sleforsap15_paygo_minion_configuration["hostnames"][0] : null
     sle11sp4_minion      = length(var.sle11sp4_minion_configuration["hostnames"]) > 0 ? var.sle11sp4_minion_configuration["hostnames"][0] : null
     sle11sp4_sshminion   = length(var.sle11sp4_sshminion_configuration["hostnames"]) > 0 ? var.sle11sp4_sshminion_configuration["hostnames"][0] : null
     sle11sp4_client      = length(var.sle11sp4_client_configuration["hostnames"]) > 0 ? var.sle11sp4_client_configuration["hostnames"][0] : null
