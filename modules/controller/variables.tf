4de9160713cb2f2c9a24c0fd2e19465b2ed10c62
Maxime Noel <maxime.noel@suse.com>
2023-12-12 09:42:53 +1300

    Modify new paygo instances name to controller

--- modules/controller/variables.tf
+++ modules/controller/variables.tf
@@ -112,22 +112,22 @@ variable "monitoringserver_configuration" {
   }
 }
 
-variable "sle12_paygo_minion_configuration" {
-  description = "use module.<SLE12_PAYGO_MINION>.configuration, see main.tf.libvirt-testsuite.example"
+variable "sle12sp5_paygo_minion_configuration" {
+  description = "use module.<SLE12SP5_PAYGO_MINION>.configuration, see main.tf.libvirt-testsuite.example"
   default = {
     hostnames = []
   }
 }
 
-variable "sle15_paygo_minion_configuration" {
-  description = "use module.<SLE15_PAYGO_MINION>.configuration, see main.tf.libvirt-testsuite.example"
+variable "sle15sp5_paygo_minion_configuration" {
+  description = "use module.<SLE15SP5_PAYGO_MINION>.configuration, see main.tf.libvirt-testsuite.example"
   default = {
     hostnames = []
   }
 }
 
-variable "sleforsap15_paygo_minion_configuration" {
-  description = "use module.<SLEFORSAP15_PAYGO_MINION>.configuration, see main.tf.libvirt-testsuite.example"
+variable "sleforsap15sp5_paygo_minion_configuration" {
+  description = "use module.<SLEFORSAP15SP5_PAYGO_MINION>.configuration, see main.tf.libvirt-testsuite.example"
   default = {
     hostnames = []
   }
