9f623d5d9537a77e14a9ff5b28b95ba55604d858
Eric Bischoff <ebischoff@suse.com>
2024-02-17 08:42:07 +0100

    Remove unused module variable
    overwrite_fqdn is defined in the provider settings

--- backend_modules/null/host/variables.tf
+++ backend_modules/null/host/variables.tf
@@ -125,9 +125,3 @@ variable "volume_provider_settings" {
   description = "Map of volume-provider-specific settings, see the backend-specific README file"
   default     = {}
 }
-
-variable "overwrite_fqdn" {
-  description = "use the specified FQDN as hostname for the system"
-  type        = string
-  default     = null
-}
