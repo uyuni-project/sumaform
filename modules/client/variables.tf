f606cc291f502270b190f51e772595f40a361dbc
Oscar Barrios <obarrios@suse.com>
2024-03-21 10:06:52 +0100

    Change the default value of install_salt_bundle to true

--- modules/client/variables.tf
+++ modules/client/variables.tf
@@ -48,7 +48,7 @@ variable "additional_packages" {
 
 variable "install_salt_bundle" {
   description = "use true to install the venv-salt-minion package in the hosts"
-  default     = false
+  default     = true
 }
 
 variable "quantity" {
