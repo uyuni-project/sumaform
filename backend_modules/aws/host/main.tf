fd1ae1e9a1afffec6891084feeb76573886a8172
Maxime Noel <maxime.noel@suse.com>
2024-05-09 15:16:50 +1200

    Fix role variables

--- backend_modules/aws/host/main.tf
+++ backend_modules/aws/host/main.tf
@@ -52,7 +52,7 @@ data "template_file" "user_data" {
     public_instance          = local.provider_settings["public_instance"]
     mirror_url               = var.base_configuration["mirror"]
     install_salt_bundle      = var.install_salt_bundle
-    roles                    = var.roles[0]
+    role                    = var.roles[0]
 #     product_version          = var.grains["product_version"] == null ? var.grains["product_version"] : "no_product_version"
   }
 }
