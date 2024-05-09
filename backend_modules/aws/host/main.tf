66228694bac149d3084e33fbc66c82613fe03dee
Maxime Noel <maxime.noel@suse.com>
2024-05-09 14:13:16 +1200

    Add salt for slemicro

--- backend_modules/aws/host/main.tf
+++ backend_modules/aws/host/main.tf
@@ -52,6 +52,8 @@ data "template_file" "user_data" {
     public_instance          = local.provider_settings["public_instance"]
     mirror_url               = var.base_configuration["mirror"]
     install_salt_bundle      = var.install_salt_bundle
+    roles                    = var.roles
+    product_version          = var.grains["product_version"]
   }
 }
 
