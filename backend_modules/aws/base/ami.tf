79ee75edb2cfeb733a6864ea7efc6490f805b69c
Maxime Noel <maxime.noel@suse.com>
2024-06-07 14:34:18 +1200

    Fix rocky name issue and remove sles15sp6 ( doesn't exist yet on aws )

--- backend_modules/aws/base/ami.tf
+++ backend_modules/aws/base/ami.tf
@@ -124,48 +124,6 @@ data "aws_ami" "sles15sp5-paygo" {
   }
 }
 
-data "aws_ami" "sles15sp6o" {
-  most_recent = true
-  name_regex  = "^suse-sles-15-sp6-byos-v"
-  owners      = ["679593333241"]
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
-data "aws_ami" "sles15sp6-paygo" {
-  most_recent = true
-  name_regex  = "^suse-sles-15-sp6-v[0-9]*-hvm"
-  owners      = ["013907871322"] // aws-marketplace
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
 data "aws_ami" "suma-server-43-byos" {
   most_recent = true
   name_regex  = "^suse-manager-server-4-3-byos-v"
@@ -297,7 +255,7 @@ data "aws_ami" "sles12sp5-paygo" {
 
 data "aws_ami" "rocky8" {
   most_recent = true
-  name_regex  = "^Rocky-8-ec2-8"
+  name_regex  = "^Rocky-8-EC2-8"
   owners      = ["792107900819"]
 
   filter {
