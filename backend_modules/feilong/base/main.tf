ccc79b4a0fa6a6cd4736d80870aff463b4f97ac7
maximenoel8 <55169628+maximenoel8@users.noreply.github.com>
2024-10-03 10:01:17 +1300

    Feilong base is directly call without using base/main.tf. (#1717)
    Update feilong base to support new product_version variable

--- backend_modules/feilong/base/main.tf
+++ backend_modules/feilong/base/main.tf
@@ -1,15 +1,17 @@
 locals {
-  name_prefix  = var.name_prefix
-  domain       = var.domain
-  ssh_key_path = var.ssh_key_path
-  key_file     = lookup(var.provider_settings, "key_file", "~/.ssh/id_rsa")
+  name_prefix       = var.name_prefix
+  domain            = var.domain
+  ssh_key_path      = var.ssh_key_path
+  key_file          = lookup(var.provider_settings, "key_file", "~/.ssh/id_rsa")
+  product_version   = var.product_version
 }
 
 output "configuration" {
   value = {
-    name_prefix  = local.name_prefix
-    domain       = local.domain
-    ssh_key_path = local.ssh_key_path
-    key_file     = local.key_file
+    name_prefix     = local.name_prefix
+    domain          = local.domain
+    ssh_key_path    = local.ssh_key_path
+    key_file        = local.key_file
+    product_version = local.product_version
   }
 }
