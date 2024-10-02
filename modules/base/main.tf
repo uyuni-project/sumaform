150380a9bf5a5bd05c5f7f7ff08519ddb28a791e
Maxime Noel <maxime.noel@suse.com>
2024-10-02 14:55:39 +1300

    Correctly update module base.

--- modules/base/main.tf
+++ modules/base/main.tf
@@ -3,6 +3,7 @@ module "base_backend" {
 
   cc_username              = var.cc_username
   cc_password              = var.cc_password
+  product_version          = var.product_version
   timezone                 = var.timezone
   use_ntp                  = var.use_ntp
   ssh_key_path             = var.ssh_key_path
