16f6faab8e1a67df38c455639fef16d5b81a0001
Maxime Noel <maxime.noel@suse.com>
2024-12-05 12:52:00 +1300

    update ami search

--- backend_modules/aws/base/ami.tf
+++ backend_modules/aws/base/ami.tf
@@ -192,7 +192,7 @@ data "aws_ami" "suma-server-43-ltd-paygo" {
 // EMEA offer
 data "aws_ami" "suma-server-50-ltd-paygo" {
   most_recent = true
-  name_regex  = "^suse-manager-server-5-0-v.*(ltd).*$"
+  name_regex  = "^SUSE-Manager-Server-5\\.0.*"
   owners      = ["679593333241"]
 
   filter {
