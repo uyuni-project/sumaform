684e299591cf9feb41befded0efcf53ecc9b10ab
Maxime Noel <55169628+maximenoel8@users.noreply.github.com>
2024-12-20 13:38:22 +1300

    Add paygo images support (#1754)

--- backend_modules/aws/network/main.tf
+++ backend_modules/aws/network/main.tf
@@ -271,7 +271,7 @@ resource "aws_security_group" "public" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = formatlist("%s/32", var.ssh_allowed_ips)
+    cidr_blocks = var.ssh_allowed_ips
   }
 
   ingress {
