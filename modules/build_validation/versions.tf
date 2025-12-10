07ad5938af9fab41a74aa114ee8b7046816e9115
Maxime Noel <maxime.noel@suse.com>
2025-12-10 14:07:19 +1300

    Remove provider duplication

--- modules/build_validation/versions.tf
+++ modules/build_validation/versions.tf
@@ -1,1 +1,21 @@
-../backend/host/versions.tf
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.8.3"
+      configuration_aliases = [
+        libvirt.host_old_sle, # For SLES 12
+        libvirt.host_new_sle, # For SLES 15/Micro
+        libvirt.host_res,     # For Alma/Rocky/CentOS/Oracle
+        libvirt.host_debian,  # For Ubuntu/Debian
+        libvirt.host_retail,  # For Proxy/BuildHosts/Terminals
+        libvirt.host_arm
+      ]
+    }
+    feilong = {
+      source  = "bischoff/feilong"
+      version = "0.0.9"
+    }
+  }
+}
