c06dde6d6ddf2b8fb4c4bb7c29fef6cae57eb8c5
Paolo Invernizzi <paolo.invernizzi@suse.com>
2026-02-18 10:08:03 +0100

    Read BV server and proxy additional repos from env. config

--- modules/build_validation/main.tf
+++ modules/build_validation/main.tf
@@ -99,8 +99,7 @@ module "server" {
   from_email                     = "root@suse.de"
   accept_all_ssl_protocols       = true
 
-  //server_additional_repos
-
+  additional_repos               = var.environment_configuration.server.additional_repos
 }
 
 module "server_containerized" {
@@ -142,8 +141,7 @@ module "server_containerized" {
   from_email                     = "root@suse.de"
   provision                      = true
 
-  //server_additional_repos
-
+  additional_repos               = var.environment_configuration.server_containerized.additional_repos
 }
 
 module "proxy" {
@@ -168,8 +166,7 @@ module "proxy" {
   use_os_released_updates   = true
   ssh_key_path              = "./salt/controller/id_ed25519.pub"
 
-  //proxy_additional_repos
-
+  additional_repos          = var.environment_configuration.proxy.additional_repos
 }
 
 module "proxy_containerized" {
@@ -191,8 +188,7 @@ module "proxy_containerized" {
   ssh_key_path         = "./salt/controller/id_ed25519.pub"
   provision            = true
 
-  //proxy_additional_repos
-
+  additional_repos     = var.environment_configuration.proxy_containerized.additional_repos
 }
 
 module "sles12sp5_minion" {
