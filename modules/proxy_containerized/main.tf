43919c968c99735ee8d8b30db6fb5e708b5af6ae
Eric Bischoff <ebischoff@suse.com>
2026-03-12 13:42:31 +0100

    Switch Head server default image to SL micro 6.2

--- modules/proxy_containerized/main.tf
+++ modules/proxy_containerized/main.tf
@@ -1,7 +1,7 @@
 variable "images" {
   default = {
-    "head"           = "slmicro61o"
-    "head-staging"   = "slmicro61o"
+    "head"           = "slmicro62o"
+    "head-staging"   = "slmicro62o"
     "5.0-released"   = "slemicro55o"
     "5.0-nightly"    = "slemicro55o"
     "5.1-released"   = "slmicro61o"
